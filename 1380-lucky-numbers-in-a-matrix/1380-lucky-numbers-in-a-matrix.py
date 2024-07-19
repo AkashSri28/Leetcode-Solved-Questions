e6dc10f57f8cb3afae89bb52479998ec1bce1ed7
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-07-19 09:31:00 +0530

    Time: 111 ms (56.85%), Space: 17 MB (15.89%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
@@ -1,29 +1,13 @@
 class Solution:
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
-        m, n = len(matrix), len(matrix[0])
-        lucky, min_in_row, max_in_col = [], [0]*m, [0]*n
-        
-        #store min_in_row
-        for i in range(0, m):
-            minimum = float('inf')
-            for j in range(0, n):
-                if matrix[i][j] < minimum:
-                    min_in_row[i] = matrix[i][j]
-                    minimum = matrix[i][j]
-        
-        #store max_in_col
-        for j in range(0, n):
-            maximum = float('-inf')
-            for i in range(0, m):
-                if matrix[i][j] > maximum:
-                    max_in_col[j] = matrix[i][j]
-                    maximum = matrix[i][j]
-                    
-        #check if curr num is lucky number
-        for i in range(0, m):
-            for j in range(0, n):
-                if matrix[i][j] == min_in_row[i] and matrix[i][j] == max_in_col[j]:
-                    lucky.append(matrix[i][j])        
-        
-        return lucky
+        # Step 1: Find the minimum element in each row
+        min_in_rows = {min(row) for row in matrix}
+
+        # Step 2: Find the maximum element in each column
+        max_in_columns = {max(col) for col in zip(*matrix)}
+
+        # Step 3: The intersection of min_in_rows and max_in_columns are the lucky numbers
+        lucky_numbers = min_in_rows & max_in_columns
+
+        return list(lucky_numbers)
         
