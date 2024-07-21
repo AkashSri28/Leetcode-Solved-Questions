8483fd2b2c381f3e0f1117e88ba1c0c4d9feb40c
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-07-21 13:58:53 +0530

    Time: 570 ms (87.79%), Space: 25.4 MB (45.80%) - LeetHub

---
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.py
@@ -0,0 +1,40 @@
+class Solution:
+    def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
+        matrix = [[0]*k for _ in range(k)]
+        
+        def topo_sort(edges):
+            graph = defaultdict(list)
+            in_degree = {i:0 for i in range(1, k+1)}
+            order_list = []
+            
+            for u,v in edges:
+                graph[u].append(v)
+                in_degree[v] += 1
+            
+            queue = deque([i for i in range(1, k+1) if in_degree[i] == 0])
+            
+            while queue:
+                node = queue.popleft()
+                order_list.append(node)
+                
+                for nei in graph[node]:
+                    in_degree[nei] -= 1
+                    if in_degree[nei] == 0:
+                        queue.append(nei)
+                        
+            return order_list                                
+        
+        row_order = topo_sort(rowConditions)
+        col_order = topo_sort(colConditions)
+        
+        if len(row_order) < k or len(col_order) < k:
+            return []
+        
+        row_position = {n:i for i,n in enumerate(row_order)}
+        col_position = {n:i for i,n in enumerate(col_order)}
+        
+        for i in range(1, k+1):
+            matrix[row_position[i]][col_position[i]] = i
+        
+        return matrix
+        
