1ec355baa92f5b35676c86cdea20f1757bde8f2f
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-08-05 09:58:01 +0530

    Time: 70 ms (59.37%), Space: 16.9 MB (35.47%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.py
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.py
@@ -1,12 +1,12 @@
 class Solution:
     def kthDistinct(self, arr: List[str], k: int) -> str:
         res, counter = "", 0
-        map = defaultdict(int)
+        map = defaultdict(int)      #O(n)
         
-        for a in arr:
+        for a in arr:       #O(n)
             map[a] += 1
             
-        for key, value in map.items():
+        for key, value in map.items():  #O(n)
             if value > 1:
                 continue
             counter += 1
@@ -16,3 +16,7 @@ class Solution:
         
         return res
         
+        # TC: O(n)
+        #     SC: O(n)
+        #         Approach: we need to store all strings as duplicate can come later as well
+        #             now check frequencies and count where frequency is 1
