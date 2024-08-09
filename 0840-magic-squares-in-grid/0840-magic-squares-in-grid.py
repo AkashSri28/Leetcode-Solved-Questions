06d4d27d5be67e3fea96086e10bdbaca887e8518
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-08-09 09:19:46 +0530

    Time: 48 ms (14.36%), Space: 16.5 MB (86.67%) - LeetHub

---
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -0,0 +1,33 @@
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        row = len(grid)
+        col = len(grid[0])
+        ans = 0
+        
+        def check_square(grid, i, j):
+            if row - 1 <= i or i < 1 or col - 1 <= j or j < 1:
+                return False
+            
+            visited = set()
+            matrix = []
+            for k in range(-1, 2):
+                rows = []
+                for l in range(-1, 2):
+                    if 1 <= grid[i+k][j+l] <= 9 and grid[i+k][j+l] not in visited:
+                        visited.add(grid[i+k][j+l])
+                        rows.append(grid[i+k][j+l])
+                    
+                if len(rows) < 3:
+                    return False
+                matrix.append(rows)
+            
+            return (matrix[0][0] + matrix[0][1] + matrix[0][2] == 15) and (matrix[1][0] + matrix[1][1] + matrix[1][2] == 15) and (matrix[2][0] + matrix[2][1] + matrix[2][2] == 15) and (matrix[0][0] + matrix[1][0] + matrix[2][0] == 15) and (matrix[0][1] + matrix[1][1] + matrix[2][1] == 15) and (matrix[0][2] + matrix[1][2] + matrix[2][2] == 15) and (matrix[0][0] + matrix[1][1] + matrix[2][2] == 15) and (matrix[0][2] + matrix[1][1] + matrix[2][0] == 15) 
+                        
+        
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == 5:
+                    if check_square(grid, i, j):
+                        ans += 1
+        return ans
+        
