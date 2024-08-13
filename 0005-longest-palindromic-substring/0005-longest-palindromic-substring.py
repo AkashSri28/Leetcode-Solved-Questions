6ae87111a473d1c9b1b348b57956253cb039ed20
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-08-13 19:30:26 +0530

    Time: 268 ms (88.32%), Space: 16.4 MB (91.56%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -0,0 +1,34 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        ans = ""
+        n = len(s)
+        
+        def check_longest_palindrome(s, i):
+            nonlocal ans
+            j = k = i
+            pal_len = 0
+            while j >= 0 and k < n and s[j] == s[k]:
+                pal_len = (k-j)+1
+                j -= 1
+                k += 1
+                
+            if pal_len > len(ans):
+                ans = s[j+1:k]
+                
+            j = i
+            k = i+1
+            pal_len = 0
+            while j >= 0 and k < n and s[j] == s[k]:
+                pal_len = (k-j)+1
+                j -= 1
+                k += 1
+                
+            if pal_len > len(ans):
+                ans = s[j+1:k]
+                
+        
+        #for every char we can check if it is center of any palindrome
+        for i in range(len(s)):
+            check_longest_palindrome(s, i)
+            
+        return ans
