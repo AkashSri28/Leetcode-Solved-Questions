e572ad269cd68ea601f20dca6c5ca397df5fbb08
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-08-29 19:34:23 +0530

    Time: 2029 ms (72.77%), Space: 32.6 MB (66.53%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,32 +1,24 @@
 class Solution:
-    def countSubIslands(self, grid1, grid2):
-        def dfs(i, j):
-            # If the current cell is out of bounds or water in grid2, return True
-            if i < 0 or i >= len(grid2) or j < 0 or j >= len(grid2[0]) or grid2[i][j] == 0:
+    def dfs(self, grid2, i, j, grid1, m, n, vis):
+        if i < 0 or i >= m or j < 0 or j >= n or grid2[i][j] == 0 or vis[i][j] == 1:
                 return True
+            
+        vis[i][j] = 1
+        res = True
+        if grid1[i][j] == 0:
+            res = False
+        return res & self.dfs(grid2, i+1, j, grid1, m, n, vis) & self.dfs(grid2, i, j-1, grid1, m, n, vis) & self.dfs(grid2, i-1, j, grid1, m, n, vis) & self.dfs(grid2, i, j+1, grid1, m, n, vis)
 
-            # Mark the current cell as visited in grid2
-            grid2[i][j] = 0
-
-            # Check if this cell in grid2 corresponds to land in grid1
-            is_sub_island = grid1[i][j] == 1
-
-            # Explore all four directions and check if they are part of the sub-island
-            is_sub_island &= dfs(i-1, j)
-            is_sub_island &= dfs(i+1, j)
-            is_sub_island &= dfs(i, j-1)
-            is_sub_island &= dfs(i, j+1)
-
-            return is_sub_island
-
-        count = 0
-        for i in range(len(grid2)):
-            for j in range(len(grid2[0])):
-                if grid2[i][j] == 1:  # Start DFS if we find land in grid2
-                    if dfs(i, j):  # Check if this island is a sub-island
-                        count += 1
-
-        return count
-
-
+        
+    
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        m, n, cnt = len(grid2), len(grid2[0]), 0
+        vis = [[0]*n for _ in range(m)]
+        
+        for i in range(m):
+            for j in range(n):
+                if grid2[i][j] == 1 and vis[i][j] == 0 and self.dfs(grid2, i, j, grid1, m, n, vis):
+                    cnt += 1
+                        
+        return cnt
         
