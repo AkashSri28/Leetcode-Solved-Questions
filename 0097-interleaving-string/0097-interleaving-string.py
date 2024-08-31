d41d2a6017cc6184a8c2d3024709957b5aaa4115
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-08-31 23:52:38 +0530

    Time: 35 ms (88.99%), Space: 18 MB (10.68%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        def func(i, j, k):
+            if i == m and j == n and k == l:
+                return True
+            if (i, j, k) not in dp:
+                a = b = False
+                if i < m and k < l and s1[i] == s3[k]:
+                    a = func(i+1, j, k+1)
+                if j < n and k < l and s2[j] == s3[k]:
+                    b = func(i, j+1, k+1)
+                dp[(i,j,k)] = a or b
+                
+            return dp[(i,j,k)]
+            
+        m, n, l = len(s1), len(s2), len(s3)
+        dp = dict()
+        dp[(m,n,l)] = True
+        
+        return func(0, 0, 0)
+        
