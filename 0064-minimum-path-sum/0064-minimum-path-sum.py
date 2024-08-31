6a3c28470af12fab006181653d6358c5543df7ac
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-08-31 12:09:57 +0530

    Time: 117 ms (14.67%), Space: 18.5 MB (23.41%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -2,15 +2,17 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
         dp = [[0]*n for _ in range(m)]      #for any i, j it stores min distance till m-1, n-1
-        for i in range(m-1, -1, -1):
-            for j in range(n-1, -1, -1):
-                dp[i][j] = grid[i][j]
-                if i+1 < m and j+1 < n:
-                    dp[i][j] += min(dp[i+1][j], dp[i][j+1])
-                elif i+1 < m:
-                    dp[i][j] += dp[i+1][j]
-                elif j+1 < n:
-                    dp[i][j] += dp[i][j+1]
+        dp[m-1][n-1] = grid[m-1][n-1]
+        
+        for i in range(n-2, -1, -1):
+            dp[m-1][i] = grid[m-1][i] + dp[m-1][i+1]
+            
+        for i in range(m-2, -1, -1):
+            dp[i][n-1] = grid[i][n-1] + dp[i+1][n-1]
+            
+        for i in range(m-2, -1, -1):
+            for j in range(n-2, -1, -1):
+                dp[i][j] = grid[i][j] + min(dp[i+1][j], dp[i][j+1])
         
         return dp[0][0]
         
