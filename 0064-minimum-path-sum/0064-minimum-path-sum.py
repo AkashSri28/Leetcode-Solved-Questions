b37079216ae2656a5e6e52f38d1b87cba4479af9
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-08-31 12:11:55 +0530

    Time: 127 ms (7.38%), Space: 18.4 MB (51.28%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,18 +1,35 @@
 class Solution:
+#       def minPathSum(self, grid: List[List[int]]) -> int:
+#         m, n = len(grid), len(grid[0])
+#         dp = [[0]*n for _ in range(m)]      #for any i, j it stores min distance till m-1, n-1
+#         dp[m-1][n-1] = grid[m-1][n-1]
+        
+#         for i in range(n-2, -1, -1):
+#             dp[m-1][i] = grid[m-1][i] + dp[m-1][i+1]
+            
+#         for i in range(m-2, -1, -1):
+#             dp[i][n-1] = grid[i][n-1] + dp[i+1][n-1]
+            
+#         for i in range(m-2, -1, -1):
+#             for j in range(n-2, -1, -1):
+#                 dp[i][j] = grid[i][j] + min(dp[i+1][j], dp[i][j+1])
+        
+#         return dp[0][0]
+
     def minPathSum(self, grid: List[List[int]]) -> int:
+            
+        
         m, n = len(grid), len(grid[0])
-        dp = [[0]*n for _ in range(m)]      #for any i, j it stores min distance till m-1, n-1
-        dp[m-1][n-1] = grid[m-1][n-1]
         
-        for i in range(n-2, -1, -1):
-            dp[m-1][i] = grid[m-1][i] + dp[m-1][i+1]
-            
-        for i in range(m-2, -1, -1):
-            dp[i][n-1] = grid[i][n-1] + dp[i+1][n-1]
-            
-        for i in range(m-2, -1, -1):
-            for j in range(n-2, -1, -1):
-                dp[i][j] = grid[i][j] + min(dp[i+1][j], dp[i][j+1])
+        for i in range(1, m):
+            grid[i][0] += grid[i-1][0]
         
-        return dp[0][0]
+        for i in range(1, n):
+            grid[0][i] += grid[0][i-1]
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        
+        return grid[-1][-1]
         
