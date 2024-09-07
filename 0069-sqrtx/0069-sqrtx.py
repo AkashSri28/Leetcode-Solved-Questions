947d886f2574c0e10f7c8f646d21d5dfee4340d6
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-09-07 16:54:01 +0530

    Time: 44 ms (36.49%), Space: 16.5 MB (66.04%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -11,4 +11,8 @@ class Solution:
                 ans = mid
                 low = mid+1
         return ans
+    
+    # TC: O(log x)
+    #     SC: O(1)
+    #         Approach: Ans will be between 0 and x//2. Now find all numbers whose square is less than our target. Take care of edge cases, 0 and 1.
         
