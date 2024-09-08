cbe56c7a3d01a0cd9e82d5a6e4392d87e6467803
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-09-08 17:32:16 +0530

    Time: 35 ms (71.96%), Space: 16.4 MB (85.11%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,29 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        carry = 0
+        i, j = len(a)-1, len(b)-1
+        ans = []
+        while i >= 0 or j >= 0 or carry > 0:
+            a1 = int(a[i]) if i >= 0 else 0
+            b1 = int(b[j]) if j >= 0 else 0
+            s = a1+b1+carry
+            match s:
+                case 0:
+                    ans.append("0")
+                    carry = 0
+                case 1:
+                    ans.append("1")
+                    carry = 0
+                case 2:
+                    ans.append("0")
+                    carry = 1
+                case 3:
+                    ans.append("1")
+                    carry = 1
+                    
+            i -= 1
+            j -= 1
+            
+        
+        return ''.join(ans)[::-1]
+        
