0fb4af0de5a726fea4a2ea390fdd84efc8c439a3
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-09-09 08:43:45 +0530

    Time: 663 ms (63.04%), Space: 54.5 MB (97.68%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
+        node = head
+        i, j, k, l = 0, n-1, m-1, 0
+        ans = [[-1]*n for _ in range(m)]
+        while i <= k and l <= j:
+            loop = l
+            while loop <= j:
+                if node:
+                    ans[i][loop] = node.val
+                    node = node.next
+                loop += 1
+            i += 1
+            
+            loop = i
+            while loop <= k:
+                if node:
+                    ans[loop][j] = node.val
+                    node = node.next
+                loop += 1
+            j -= 1
+            
+            loop = j
+            while loop >= l:
+                if node:
+                    ans[k][loop] = node.val
+                    node = node.next
+                loop -= 1
+            k -= 1
+                    
+            loop = k
+            while loop >= i:
+                if node:
+                    ans[loop][l] = node.val
+                    node = node.next
+                loop -= 1
+            l += 1            
+                    
+        return ans
+                
+        
+        
