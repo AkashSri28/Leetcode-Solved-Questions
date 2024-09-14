b6cbead3cc8210bc8d595d69a6d7c6b7fe34dcbc
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-09-14 22:19:12 +0530

    Time: 761 ms (5.46%), Space: 31.2 MB (42.18%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.py
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.py
@@ -1,39 +1,40 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        MAX = max(nums) ### Find the largest number in nums.
-        res = 0			 
-        count = 0		### Used to count the length of the continues subarray that only contains MAX
-        for n in nums:
-        	### If the current number is MAX, increase count
-            if n==MAX:
-                count +=1
-            ### Otherwise, reset count.
-            else:
-                count = 0
-            ### store the maximum count as result.
-            res = max(res, count)
-        return res
-#         n = len(nums)
-#         res = [0]*n
-#         res[0] = nums[0]
+        # MAX = max(nums) ### Find the largest number in nums.
+        # res = 0			 
+        # count = 0		### Used to count the length of the continues subarray that only contains MAX
+        # for n in nums:
+        # 	### If the current number is MAX, increase count
+        #     if n==MAX:
+        #         count +=1
+        #     ### Otherwise, reset count.
+        #     else:
+        #         count = 0
+        #     ### store the maximum count as result.
+        #     res = max(res, count)
+        # return res
+        n = len(nums)
+        res = [0]*n
+        res[0] = nums[0]
         
-#         for i in range(1, n):
-#             temp = res[i-1] & nums[i]
-#             res[i] = max(nums[i], temp)
+        for i in range(1, n):
+            temp = res[i-1] & nums[i]
+            res[i] = max(nums[i], temp)
             
-#         maxi, cnt, ans = res[0], 1, 1
+        maxi, cnt, ans = res[0], 1, 1
         
-#         for i in range(1, n):
-#             if maxi == res[i]:
-#                 cnt += 1
-#                 ans = max(ans, cnt)
-#             elif maxi < res[i]:
-#                 maxi = res[i]
-#                 cnt = 1
-#             else:
-#                 cnt = 0
+        for i in range(1, n):
+            if maxi == res[i]:
+                cnt += 1
+                ans = max(ans, cnt)
+            elif maxi < res[i]:
+                maxi = res[i]
+                cnt = 1
+                ans = 1
+            else:
+                cnt = 0
                 
-#         return ans
+        return ans
 
             
         
