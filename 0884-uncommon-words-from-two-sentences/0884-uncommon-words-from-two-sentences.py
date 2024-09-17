6ff12286e2086deef48a0c337e82c979623b5226
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-09-17 09:41:20 +0530

    Time: 45 ms (5.64%), Space: 16.6 MB (5.76%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
@@ -1,17 +1,20 @@
 class Solution:
-    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
-        s1_set, s2_set = set(), set()
-        for word in s1.split(' '):
-            if word in s1_set:
-                s2_set.add(word)
-            else:
-                s1_set.add(word)
+    def uncommonFromSentences(self, A, B):
+        c = collections.Counter((A + " " + B).split())
+        return [w for w in c if c[w] == 1]
+#     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+#         s1_set, s2_set = set(), set()
+#         for word in s1.split(' '):
+#             if word in s1_set:
+#                 s2_set.add(word)
+#             else:
+#                 s1_set.add(word)
                 
-        for word in s2.split(' '):
-            if word in s2_set:
-                s1_set.add(word)
-            else:
-                s2_set.add(word)
-        ans = s1_set.symmetric_difference(s2_set)
-        return ans
+#         for word in s2.split(' '):
+#             if word in s2_set:
+#                 s1_set.add(word)
+#             else:
+#                 s2_set.add(word)
+#         ans = s1_set.symmetric_difference(s2_set)
+#         return ans
         
