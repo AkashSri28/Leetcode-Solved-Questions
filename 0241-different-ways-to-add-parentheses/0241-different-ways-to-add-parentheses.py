b0bc99af1c363697c28f5f207457a873bb66e1c9
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-09-19 08:37:03 +0530

    Time: 39 ms (51.35%), Space: 16.8 MB (27.58%) - LeetHub

---
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -0,0 +1,25 @@
+class Solution:
+    operators = {
+        '+': lambda x, y: x+y,
+        '*': lambda x, y: x*y,
+        '-': lambda x, y: x-y
+    }
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        res = []
+        
+        for i in range(len(expression)):
+            ch = expression[i]
+            if ch in self.operators:
+                nums1 = self.diffWaysToCompute(expression[:i])
+                nums2 = self.diffWaysToCompute(expression[i+1:])
+                
+                for n1 in nums1:
+                    for n2 in nums2:
+                        res.append(self.operators[ch](n1, n2))
+                        
+        if not res:
+            res.append(int(expression))
+                
+        
+        return res
+        
