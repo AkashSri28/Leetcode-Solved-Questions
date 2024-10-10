332c3f7f918a2da21249656ed7afbf8efe830b29
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-10-10 09:37:53 +0530

    Time: 392 ms (20.67%), Space: 23.5 MB (61.70%) - LeetHub

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.py
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.py
@@ -14,9 +14,12 @@ class Solution:
                 if nums[i] <= nums[j]:
                     ans = max(ans, j-i)
                 j += 1
-                
-                    
+                              
         return ans
+    
+    # TC: O(n)
+    #     SC: O(n)
+    #         Approach: We need to know if there exists an answer to the right. We can store max element on right and if its greater than current, that means an answer exists.
                 
                 
         
