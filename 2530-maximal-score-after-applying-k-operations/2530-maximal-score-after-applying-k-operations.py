c6d9a51cbfdf2819bcbbf8e1f35a616581dd9c92
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-10-14 09:57:17 +0530

    Time: 799 ms (12.02%), Space: 31.2 MB (55.06%) - LeetHub

--- 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.py
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.py
@@ -3,17 +3,21 @@ class Solution:
         max_heap = []   #(num, idx)
         
         for i, n in enumerate(nums):
-            heapq.heappush(max_heap, (-1*n, i))
+            heapq.heappush(max_heap, -1*n)
             
         score = 0
         while k > 0:
             k -= 1
-            num, idx = heapq.heappop(max_heap)
+            num = heapq.heappop(max_heap)
             num = -1*num
             score += num
             num = math.ceil(num/3)
-            heapq.heappush(max_heap, (-1*num, idx))
+            heapq.heappush(max_heap, -1*num)
             
         return score
+    
+    # TC: O(k*logn)
+    #     SC: O(n)
+            
             
         
