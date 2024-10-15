affd73393769d9caaef6bf9332bfb8a42609b80b
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-10-15 09:49:04 +0530

    Time: 114 ms (37.61%), Space: 17.5 MB (83.58%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.py
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.py
@@ -1,11 +1,11 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
-        s = list(s)
+        # s = list(s)
         j = ans = 0
         for i in range(len(s)):
             if s[i] == '0':
                 ans += (i-j)
-                s[i], s[j] = s[j], s[i]
+                # s[i], s[j] = s[j], s[i]
                 j += 1
                 
         return ans
