8e5258f7836510f940fcbde25d199aaf1f6f247a
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-10-18 06:53:48 +0530

    Time: 459 ms (43.98%), Space: 17.2 MB (28.18%) - LeetHub

---
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        max_val = float('-inf')
+        mem = defaultdict(int)
+        
+        def helper(index, val):
+            nonlocal max_val
+            if index == len(nums):
+                max_val = max(max_val, val)
+                mem[val] += 1
+                return
+                
+            helper(index+1, val|nums[index])
+            helper(index+1, val)
+        
+        helper(0, 0)
+                
+        return mem[max_val]
+        
