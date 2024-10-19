d016d798a67e88a11eca3f300f9db6a6c34369ab
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-10-19 11:17:20 +0530

    Time: 0 ms (100.00%), Space: 16.6 MB (56.21%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -1,20 +1,44 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
-        if k == 1:
-            return '0'
-        s = '0'
+        def flip(n):
+            if n == '1':
+                return '0'
+            return '1'
         
-        while len(s) < k:
-            s = s+'1'
-            j = len(s) - 2
-            while j >= 0:
-                if s[j] == '1':
-                    s = s+'0'
-                else:
-                    s = s+'1'
-                j -= 1
+        def helper(n, k):
+            if n == 1:
+                return '0'
+            
+            len_s = 2**n - 1
+            mid = len_s//2 + 1
+            if k == mid:
+                return '1'
+            elif k < mid:
+                return helper(n-1, k)
+            else:
+                return flip(helper(n-1, 1+(len_s)-k))
+            
                 
-        return s[k-1]
+        
+        return helper(n, k)
+        
+        
+#         if k == 1:
+#             return '0'
+        
+#         s = '0'
+        
+#         while len(s) < k:
+#             s = s+'1'
+#             j = len(s) - 2
+#             while j >= 0:
+#                 if s[j] == '1':
+#                     s = s+'0'
+#                 else:
+#                     s = s+'1'
+#                 j -= 1
+                
+#         return s[k-1]
     
     # TC: O(k**2)
     #     SC: O(k)
