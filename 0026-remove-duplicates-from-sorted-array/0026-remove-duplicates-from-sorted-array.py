dbc89c35e9cdc7d3c1791b0c5abc16afc41568f4
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-10-22 10:53:17 +0530

    Time: 7 ms (96.65%), Space: 18 MB (53.50%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,9 +1,9 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        def swap(nums, i, j):
-            temp = nums[i]
-            nums[i] = nums[j]
-            nums[j] = temp
+        # def swap(nums, i, j):
+        #     temp = nums[i]
+        #     nums[i] = nums[j]
+        #     nums[j] = temp
             
         i, j = -1, 0
         seen = set()
@@ -11,7 +11,8 @@ class Solution:
             if nums[j] not in seen:
                 seen.add(nums[j])
                 i += 1
-                swap(nums, i, j)
+                # swap(nums, i, j)
+                nums[i], nums[j] = nums[j], nums[i]
             j += 1
             
         return i+1
