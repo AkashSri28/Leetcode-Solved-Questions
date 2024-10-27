b9ccb6d04e3a1818f2e95767b094f60f226c25c5
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-10-27 10:05:15 +0530

    Time: 59 ms (72.46%), Space: 19.5 MB (15.31%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -7,11 +7,11 @@ class Solution:
         
         for i in range(m):
             for j in range(n):
-                if i == 0 or j == 0:
-                    sq_cnt[i][j] = matrix[i][j]
-                    ans += sq_cnt[i][j]
-                    continue
                 if matrix[i][j] == 1:
+                    if i == 0 or j == 0:
+                        sq_cnt[i][j] = matrix[i][j]
+                        ans += sq_cnt[i][j]
+                        continue
                     sq_cnt[i][j] = 1+min(sq_cnt[i-1][j-1], sq_cnt[i-1][j], sq_cnt[i][j-1])
                     ans += sq_cnt[i][j]
         
