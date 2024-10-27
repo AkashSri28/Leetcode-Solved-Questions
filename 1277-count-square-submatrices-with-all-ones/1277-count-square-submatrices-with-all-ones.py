0bb9d5adb1c5d9b53ea4375136218327d0cfc1b4
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-10-27 10:07:32 +0530

    Time: 55 ms (86.96%), Space: 19.3 MB (20.45%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -2,20 +2,37 @@ class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
         m, n = len(matrix), len(matrix[0])
         
-        sq_cnt = [[0]*n for _ in range(m)]
         ans = 0
         
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 1:
                     if i == 0 or j == 0:
-                        sq_cnt[i][j] = matrix[i][j]
-                        ans += sq_cnt[i][j]
+                        ans += matrix[i][j]
                         continue
-                    sq_cnt[i][j] = 1+min(sq_cnt[i-1][j-1], sq_cnt[i-1][j], sq_cnt[i][j-1])
-                    ans += sq_cnt[i][j]
+                    matrix[i][j] = 1+min(matrix[i-1][j-1], matrix[i-1][j], matrix[i][j-1])
+                    ans += matrix[i][j]
         
         return ans
+        
+#         m, n = len(matrix), len(matrix[0])
+        
+#         sq_cnt = [[0]*n for _ in range(m)]
+#         ans = 0
+        
+#         for i in range(m):
+#             for j in range(n):
+#                 if matrix[i][j] == 1:
+#                     if i == 0 or j == 0:
+#                         sq_cnt[i][j] = matrix[i][j]
+#                         ans += sq_cnt[i][j]
+#                         continue
+#                     sq_cnt[i][j] = 1+min(sq_cnt[i-1][j-1], sq_cnt[i-1][j], sq_cnt[i][j-1])
+#                     ans += sq_cnt[i][j]
+        
+#         return ans
+    
+    
     
     # TC: O(m*n)
     #     SC: O(m*n)
