a84694cd7ef27c54857ad250fc5dbd2f0a1d3792
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-10-29 09:29:13 +0530

    Time: 143 ms (54.55%), Space: 43.6 MB (5.30%) - LeetHub

---
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.py
@@ -0,0 +1,41 @@
+class Solution:
+    def maxMoves(self, grid: List[List[int]]) -> int:
+        m, n, dirs = len(grid), len(grid[0]), [(0, 1), (1, 1), (-1, 1)]
+        @cache
+        def dp(i, j):
+            ans = 0
+            for x, y in dirs:
+                ni, nj = i + x, j + y
+                if 0 <= ni < m and nj < n and grid[i][j] < grid[ni][nj]:
+                    ans = max(ans, 1 + dp(ni, nj))
+            return ans
+        return max(dp(i, 0) for i in range(m))
+        
+#         rows, cols = len(grid), len(grid[0])
+#         dp = [[-1]*cols for _ in range(rows)]
+        
+#         def check(i, j, k, l):
+#             if k == rows or l == cols or k < 0:
+#                 return 0
+#             if grid[k][l] > grid[i][j]:
+#                 return 1+calc_moves(k, l)
+#             return 0
+        
+#         def calc_moves(i, j):
+#             if i == rows or j == cols or i < 0:
+#                 return 0
+#             if dp[i][j] == -1:
+#                 top = check(i, j, i-1, j+1)
+#                 right = check(i, j, i, j+1)
+#                 bottom = check(i, j, i+1, j+1)
+#                 dp[i][j] = max(top,right, bottom)               
+                
+#             return dp[i][j]
+        
+#         ans = float('-inf')
+#         for r in range(rows-1, -1, -1):
+#             for c in range(cols-1, -1, -1):
+#                 ans = max(ans, calc_moves(r, c))
+                
+#         return ans
+        
