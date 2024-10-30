49b35cb451c7663edd72ead7e458b78374e7b452
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-10-30 12:17:48 +0530

    Time: 1902 ms (18.60%), Space: 17 MB (13.17%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
@@ -1,64 +1,24 @@
 class Solution:
-    def minimumMountainRemovals(self, nums: List[int]) -> int:
+    def minimumMountainRemovals(self, nums: List[int]) -> int:        
         n = len(nums)
-    
-        # Step 1: Compute LIS ending at each index
-        inc = [1] * n
+        dp_left = [0]*n
+        dp_right = [0]*n
+        length = 0
+        
         for i in range(1, n):
             for j in range(i):
                 if nums[j] < nums[i]:
-                    inc[i] = max(inc[i], inc[j] + 1)
-
-        # Step 2: Compute LDS starting at each index
-        dec = [1] * n
-        for i in range(n - 2, -1, -1):
-            for j in range(i + 1, n):
+                    dp_left[i] = max(dp_left[i], 1+dp_left[j])
+        
+        for i in range(n-2, -1, -1):
+            for j in range(i+1, n):
                 if nums[j] < nums[i]:
-                    dec[i] = max(dec[i], dec[j] + 1)
-
-        # Step 3: Find the longest bitonic subsequence
-        max_bitonic_length = 0
-        for i in range(1, n - 1):  # The peak cannot be the first or last element
-            if inc[i] > 1 and dec[i] > 1:  # Valid peak
-                max_bitonic_length = max(max_bitonic_length, inc[i] + dec[i] - 1)
-
-        # Step 4: Minimum removals to make a mountain array
-        return n - max_bitonic_length
+                    dp_right[i] = max(dp_right[i], 1+dp_right[j])
         
-#         n = len(nums)
-#         dp_left = [-1]*n
-#         dp_right = [-1]*n
-#         ans = float('inf')
-        
-#         def find_max_left(i):
-#             if i == 0:
-#                 return 0
-#             if dp_left[i] == -1:
-#                 res = 0
-#                 for j in range(i-1, -1, -1):
-#                     if nums[j] < nums[i]:
-#                         res = max(res, 1+find_max_left(j))
-#                 dp_left[i] = res
-#             return dp_left[i]
-        
-#         def find_max_right(i):
-#             if i == n-1:
-#                 return 0
-#             if dp_right[i] == -1:
-#                 res = 0
-#                 for j in range(i+1, n):
-#                     if nums[j] < nums[i]:
-#                         res = max(res, 1+find_max_right(j))
-#                 dp_right[i] = res
-#             return dp_right[i]
-        
-#         for i in range(n):
-#             left = find_max_left(i)
-#             right = find_max_right(i)
-#             height = 1+left+right
-#             if 0 < i < n-1:
-#                 ans = min(ans, n-height)
+        for i in range(1, n-1):
+            if dp_left[i] > 0 and dp_right[i] > 0:
+                length = max(length, 1+dp_left[i]+dp_right[i])
             
-#         return ans
+        return n-length
             
         
