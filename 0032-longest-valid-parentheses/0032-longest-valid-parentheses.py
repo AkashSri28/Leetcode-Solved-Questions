b9b18e3e9e2b92b24bbde3bf9c7681e4c9a2e4bd
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-10-31 19:07:10 +0530

    Time: 3 ms (98.16%), Space: 16.7 MB (80.89%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -7,20 +7,19 @@ class Solution:
         def helper(i):
             if i <= 0:
                 return 0
-            if s[i] == '(':
-                return 0
             if s[i-1] == '(':
-                dp[i] = 2+helper(i-2)
+                dp[i] = 2+dp[i-2] if i-2 >= 0 else 2 
             else:
                 length = dp[i-1]
                 if length == 0:
                     return 0
                 if i-length-1 >= 0 and s[i-length-1] == '(':
-                    dp[i] = 2+length+helper(i-length-2)
+                    dp[i] = 2+length+dp[i-length-2]
             return dp[i]
             
         for index, ch in enumerate(s):
-            ans = max(ans, helper(index))
+            if ch == ')':
+                ans = max(ans, helper(index))
                 
         return ans
             
