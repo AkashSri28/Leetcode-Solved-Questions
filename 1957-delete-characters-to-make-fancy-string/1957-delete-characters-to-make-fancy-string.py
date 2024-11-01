24662fa4deeac5eb3032264e2636696ad49074e0
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-11-01 13:42:38 +0530

    Time: 292 ms (68.47%), Space: 18.5 MB (73.10%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
@@ -1,12 +1,21 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        n = len(s)
-        new_s = ""
-        for i in range(n):
-            if i >= 2 and s[i] == s[i-1] and s[i] == s[i-2]:
-                continue
-            new_s = new_s + s[i]
-        return new_s
+        result = []
+
+        for ch in s:
+            # Check if the last two characters in the result are the same as the current character
+            if len(result) >= 2 and result[-1] == ch and result[-2] == ch:
+                continue  # Skip the character if it would make three consecutive characters
+            result.append(ch)
+
+        return ''.join(result)
+        # n = len(s)
+        # new_s = ""
+        # for i in range(n):
+        #     if i >= 2 and s[i] == s[i-1] and s[i] == s[i-2]:
+        #         continue
+        #     new_s = new_s + s[i]
+        # return new_s
     
     # TC: O(n)
     #     SC: O(1)
