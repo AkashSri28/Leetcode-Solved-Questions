0072b10e8a925bbb20b761e4c143b26e3a6a059c
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-11-02 08:43:32 +0530

    Time: 1 ms (16.54%), Space: 16.5 MB (45.68%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.py
+++ 2490-circular-sentence/2490-circular-sentence.py
@@ -6,4 +6,8 @@ class Solution:
                 return False
             
         return words[-1][-1] == words[0][0]
+    
+    # TC: O(n)    n: list of words
+    #         SC: O(n)
+    #             Approach: for all consecutive words, compare last and first char of adjacent words. At last, check first and last
         
