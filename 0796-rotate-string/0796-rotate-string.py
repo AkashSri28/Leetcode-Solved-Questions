a602a4234bdb57897b33f6db6d766ad241ba5d7a
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-11-03 15:07:33 +0530

    Time: 0 ms (100.00%), Space: 16.4 MB (77.64%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -3,25 +3,28 @@ class Solution:
         if len(s) != len(goal):
             return False
         
-        indices = []
-        c = s[0]
-        n = len(s)
+        return goal in s+s
         
-        def check(i):
-            for ch in s:
-                if goal[i] != ch:
-                    return False
-                i = (i+1)%n
-            return True
+        
+#         indices = []
+#         c = s[0]
+#         n = len(s)
+        
+#         def check(i):
+#             for ch in s:
+#                 if goal[i] != ch:
+#                     return False
+#                 i = (i+1)%n
+#             return True
                     
-        for index, ch in enumerate(goal):
-            if ch == c:
-                indices.append(index)
+#         for index, ch in enumerate(goal):
+#             if ch == c:
+#                 indices.append(index)
                 
-        for i in indices:
-            if check(i):
-                return True
-        return False
+#         for i in indices:
+#             if check(i):
+#                 return True
+#         return False
     
     # TC: O(n) + O(n) Finding indices and checking strings
     #     SC: O(n)    Storing indices
