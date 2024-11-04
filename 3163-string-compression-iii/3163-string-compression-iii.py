b90b01036c2453aa07f2c983b753d3a48bbb1ce4
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-11-04 11:08:05 +0530

    Time: 165 ms (85.11%), Space: 18.9 MB (90.27%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.py
+++ 3163-string-compression-iii/3163-string-compression-iii.py
@@ -1,19 +1,34 @@
 class Solution:
     def compressedString(self, word: str) -> str:
-        comp = []
-        i = 0
-        while i < len(word):
-            curr = word[i]
-            j = i
-            while j < len(word) and word[j] == curr and j-i < 9:
-                j += 1
+        comp = ""
+        curr = word[0]
+        cnt = 1
+        for i in range(1, len(word)):
+            if curr == word[i] and cnt < 9:
+                cnt += 1
+            else:
+                comp += str(cnt) + curr
+                curr = word[i]
+                cnt = 1
+        
+        comp += str(cnt) + curr
+        return comp
                 
-            comp.append(str(j-i))
-            comp.append(curr)
+        
+#         comp = []
+#         i = 0
+#         while i < len(word):
+#             curr = word[i]
+#             j = i
+#             while j < len(word) and word[j] == curr and j-i < 9:
+#                 j += 1
+                
+#             comp.append(str(j-i))
+#             comp.append(curr)
             
-            i = j
+#             i = j
             
-        return ''.join(comp)       
+#         return ''.join(comp)       
     
     # TC: O(n)
     #     SC: O(n)
