a0b6709568b700761574c6f60c3de80db91c41dd
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-11-04 11:02:10 +0530

    Time: 242 ms (53.19%), Space: 28.5 MB (8.56%) - LeetHub

---
+++ 3163-string-compression-iii/3163-string-compression-iii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def compressedString(self, word: str) -> str:
+        comp = []
+        i = 0
+        while i < len(word):
+            curr = word[i]
+            j = i
+            while j < len(word) and word[j] == curr and j-i < 9:
+                j += 1
+                
+            comp.append(str(j-i))
+            comp.append(curr)
+            
+            i = j
+            
+        return ''.join(comp)        
