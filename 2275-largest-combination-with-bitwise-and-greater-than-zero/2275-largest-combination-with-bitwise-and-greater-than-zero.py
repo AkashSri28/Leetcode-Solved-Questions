34d93ac39acda9b70adc1c415f6648daeacd800f
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-11-07 09:43:51 +0530

    Time: 655 ms (21.25%), Space: 26.8 MB (60.30%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.py
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.py
@@ -1,17 +1,19 @@
 class Solution:
     def largestCombination(self, nums: List[int]) -> int:
-        bit_map = defaultdict(list)
+        bit_map = defaultdict(int)
         for n in nums:
             pos = 1
             while n > 0:
                 if n & 1 == 1:
-                    bit_map[pos].append(n)
+                    bit_map[pos] += 1
                 pos += 1
                 n = n >> 1
         
-        ans = 0
-        for v in bit_map.values():
-            ans = max(ans, len(v))
+        ans = max(bit_map.values())
             
         return ans
+    
+#     TC: O(2n)
+#         SC: O(n)
+#             Approach: max length queue will be where same bits are set. Use dict to store length of each and find max in end.
         
