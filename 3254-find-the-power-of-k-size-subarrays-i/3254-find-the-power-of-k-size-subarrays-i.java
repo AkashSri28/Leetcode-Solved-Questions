7afc43820aca7c6880c4dec45777e5eff3704687
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-11-16 13:06:05 +0530

    Time: 1 ms (100.00%), Space: 45.2 MB (81.91%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
@@ -1,39 +1,49 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
         int n = nums.length;
-        int[] res = new int[n-k+1];
-        int cnt = 0, index = 0;
-        
-        for(int i=1;i<k;i++){
-            if(nums[i] == nums[i-1]+1){
-                cnt += 1;
+
+        // Edge case: if k > n or nums is empty
+        if (k > n || nums == null || nums.length == 0) {
+            return new int[0];
+        }
+
+        int[] res = new int[n - k + 1];
+        int consecutiveCount = 0, resultIndex = 0;
+
+        // Initialize the sliding window
+        for (int i = 1; i < k; i++) {
+            if (isConsecutive(nums[i - 1], nums[i])) {
+                consecutiveCount++;
             }
         }
-        if(cnt == k-1){
-            res[index++] = nums[k-1];
+
+        // Check the first window
+        res[resultIndex++] = (consecutiveCount == k - 1) ? nums[k - 1] : -1;
+
+        // Sliding window
+        int left = 0, right = k;
+        while (right < n) {
+            // Update the consecutive count for the sliding window
+            if (isConsecutive(nums[left], nums[left + 1])) {
+                consecutiveCount--;
+            }
+            if (isConsecutive(nums[right - 1], nums[right])) {
+                consecutiveCount++;
+            }
+
+            // Add result for the current window
+            res[resultIndex++] = (consecutiveCount == k - 1) ? nums[right] : -1;
+
+            // Move the window
+            left++;
+            right++;
         }
-        else{
-            res[index++] = -1;
-        }
-        
-        int i=0, j=k;
-        while(j<n){
-            if(nums[i] == nums[i+1]-1){
-                cnt -= 1;
-            }
-            if(nums[j] == nums[j-1]+1){
-                cnt += 1;
-            }
-            if(cnt == k-1){
-                res[index++] = nums[j];
-            }
-            else{
-                res[index++] = -1;
-            }
-            i += 1;
-            j += 1;
-        }
-        
+
         return res;
+    }
+
+    // Helper function to check if two numbers are consecutive
+    private boolean isConsecutive(int a, int b) {
+        return b == a + 1;
     }
 }
