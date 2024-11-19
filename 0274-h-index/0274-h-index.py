f7619900c324b8997c52ee17b22871431f34afc9
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-11-19 09:11:03 +0530

    Time: 3 ms (29.18%), Space: 17 MB (45.36%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -6,4 +6,8 @@ class Solution:
             if c >= index+1:
                 h_index = index+1
         return h_index
+    
+    # TC: O(nlogn)
+    #     SC: O(1)
+    #         Approach: paper count is index+1, we try to increase paper count to the point where c is just greater than or equal to paper count. We could have used binary search here as well.
         
