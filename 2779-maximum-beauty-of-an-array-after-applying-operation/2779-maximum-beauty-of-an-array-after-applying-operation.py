d62ff48e816eda38a66215c0b9d30ce01d1bbee8
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-12-11 08:34:01 +0530

    Time: 813 ms (14.52%), Space: 46.1 MB (8.08%) - LeetHub

---
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maximumBeauty(self, nums: List[int], k: int) -> int:
+        ls = []
+        ans = float('-inf')
+        
+        for n in nums:
+            ls.append((n-k, -1))
+            ls.append((n+k, 1))
+            
+        ls.sort()
+        
+        cnt = 0
+        for num, f in ls:
+            if f == -1:
+                cnt += 1
+                ans = max(ans, cnt)
+            else:
+                cnt -= 1
+                
+        return ans
+        
