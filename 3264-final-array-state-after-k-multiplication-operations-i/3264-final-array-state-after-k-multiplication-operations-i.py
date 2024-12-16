d9806729f6124b7d4e9f172656808d163d2a2913
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-12-16 09:13:21 +0530

    Time: 0 ms (100.00%), Space: 17.2 MB (34.31%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.py
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.py
@@ -1,19 +1,16 @@
 class Solution:
-    def find_min_idx(self, nums):
-        min_ele, min_idx = nums[0], 0
-        for i in range(1, len(nums)):
-            if nums[i] < min_ele:
-                min_ele = nums[i]
-                min_idx = i
-
-        return min_idx
-
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
-        min_idx = -1
-        while k > 0:
-            min_idx = self.find_min_idx(nums)
-            nums[min_idx] *= multiplier
-            k -= 1
-
+        heap = []
+        res = [0]*len(nums)
+        
+        for index, num in enumerate(nums):
+            heapq.heappush(heap, (num, index))
+            
+        for _ in range(k):
+            num, index = heapq.heappop(heap)
+            num = num*multiplier
+            heapq.heappush(heap, (num, index))
+            nums[index] = num
+            
         return nums
         
