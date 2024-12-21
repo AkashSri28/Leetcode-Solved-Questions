ff58e7d05822133c24e3b226129d45240020db3d
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2024-12-21 17:33:34 +0530

    Time: 140 ms (62.20%), Space: 40.7 MB (10.98%) - LeetHub

---
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
+        adj = defaultdict(list)
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+            
+        res = 0
+                
+        def postOrder(node, parent):
+            nonlocal res
+            total = 0
+            for child in adj[node]:
+                if child != parent:
+                    total += postOrder(child, node)
+                    
+            total += values[node]
+            if total % k == 0:
+                res += 1
+                
+            return total
+                
+        postOrder(0, -1)
+        return res
+            
+            
+        
+        
