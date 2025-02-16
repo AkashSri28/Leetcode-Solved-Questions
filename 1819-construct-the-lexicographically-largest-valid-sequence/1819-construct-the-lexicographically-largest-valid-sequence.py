0e46a1163c7d3e52015328d4c5ac3282cd472a61
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-02-16 22:06:46 +0530

    Time: 3 ms (95.31%), Space: 18 MB (21.88%) - LeetHub

---
+++ 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.py
@@ -0,0 +1,34 @@
+class Solution:
+    def constructDistancedSequence(self, n: int) -> List[int]:
+        res = [0]*(2*n-1)
+        used = [False]*(n+1)
+
+        def backtrack(index):
+            while index < len(res) and res[index] != 0:
+                index += 1
+            if index == len(res):
+                return True
+
+            for i in range(n, 0, -1):
+                if used[i]:
+                    continue
+                if i == 1 or (index+i < len(res) and res[index+i] == 0):
+                    used[i] = True
+                    res[index] = i
+                    if i > 1:
+                        res[index+i] = i
+
+                    if backtrack(index+1):
+                        return True
+                    
+                    used[i] = False
+                    res[index] = 0
+                    if i > 1:
+                        res[index+i] = 0
+
+            return False
+
+        backtrack(0)
+
+        return res
+        
