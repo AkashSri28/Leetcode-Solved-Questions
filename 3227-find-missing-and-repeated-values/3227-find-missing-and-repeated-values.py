3a13f5877ac97792c87b83c9ab0b264f6e23e499
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-03-06 21:24:58 +0530

    Time: 3 ms (94.14%), Space: 18.1 MB (63.86%) - LeetHub

---
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        n = len(grid)
+        mem = [0]*(n**2+1)
+        for i in range(n):
+            for j in range(n):
+                num = grid[i][j]
+                mem[num] += 1
+
+        ans = [0]*2
+        for i in range(1, n**2+1):
+            if mem[i] == 1:
+                continue
+            elif mem[i] == 2:
+                ans[0] = i
+            else:
+                ans[1] = i
+
+        return ans
+            
+
+        
