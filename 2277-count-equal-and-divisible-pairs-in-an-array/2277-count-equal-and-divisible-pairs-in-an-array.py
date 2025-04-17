83e06011d71414cfa58d088a58d5d65bf9af76f7
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-04-17 08:41:29 +0530

    Time: 7 ms (95%), Space: 17.9 MB (32.33%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.py
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.py
@@ -10,16 +10,28 @@ class Solution:
 
         # return cnt
 
-        index_map = defaultdict(list)
-        for idx, num in enumerate(nums):
-            index_map[num].append(idx)
+        # index_map = defaultdict(list)
+        # for idx, num in enumerate(nums):
+        #     index_map[num].append(idx)
 
+        # cnt = 0
+        # for indicies in index_map.values():
+        #     for i in range(len(indicies)):
+        #         for j in range(i+1, len(indicies)):
+        #             if (indicies[i]*indicies[j])%k == 0:
+        #                 cnt += 1 
+
+        # return cnt
+
+        index_map = defaultdict(list)
         cnt = 0
-        for indicies in index_map.values():
-            for i in range(len(indicies)):
-                for j in range(i+1, len(indicies)):
-                    if (indicies[i]*indicies[j])%k == 0:
-                        cnt += 1 
+        n = len(nums)
+
+        for i in range(n):
+            for j in index_map[nums[i]]:
+                if (i*j)%k == 0:
+                    cnt += 1
+            index_map[nums[i]].append(i)
 
         return cnt
         
