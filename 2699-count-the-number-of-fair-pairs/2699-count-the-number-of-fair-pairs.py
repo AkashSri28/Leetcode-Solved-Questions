fee76bbda2afbe93b47d044e072c395abdce9e82
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-04-19 09:07:29 +0530

    Time: 107 ms (88.93%), Space: 31.4 MB (86.76%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
@@ -1,27 +1,42 @@
 class Solution:
+    # def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+    #     nums.sort()
+    #     n = len(nums)
+    #     cnt = 0
+
+    #     def binary_search(l, r, target):
+    #         res = n
+    #         while l <= r:
+    #             mid = (l+r)//2
+    #             x = nums[mid]
+    #             if x >= target:
+    #                 res = mid
+    #                 r = mid-1
+    #             else:
+    #                 l = mid+1
+    #         return res
+
+
+    #     for i in range(n-1):
+    #         L = binary_search(i+1, n-1, lower-nums[i])
+    #         R = binary_search(L, n-1, upper-nums[i]+1)
+    #         cnt += (R-L)
+
+    #     return cnt
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
         nums.sort()
-        n = len(nums)
-        cnt = 0
+        return self.countPairs(nums, upper) - self.countPairs(nums, lower-1)
+    
+    def countPairs(self, nums, target):
+        count = 0
+        left, right = 0, len(nums)-1
 
-        def binary_search(l, r, target):
-            res = n
-            while l <= r:
-                mid = (l+r)//2
-                x = nums[mid]
-                if x >= target:
-                    res = mid
-                    r = mid-1
-                else:
-                    l = mid+1
-            return res
-
-
-        for i in range(n-1):
-            L = binary_search(i+1, n-1, lower-nums[i])
-            R = binary_search(L, n-1, upper-nums[i]+1)
-            cnt += (R-L)
-
-        return cnt
+        while left < right:
+            if nums[left] + nums[right] > target:
+                right -= 1
+            else:
+                count += right-left
+                left += 1
+        return count
 
         
