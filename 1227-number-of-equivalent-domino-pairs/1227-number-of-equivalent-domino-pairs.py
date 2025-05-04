6e8cb52955204d409d78d7739a246f6e3476b444
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-05-04 06:55:08 +0530

    Time: 7 ms (92.01%), Space: 24.4 MB (43.37%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.py
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.py
@@ -9,4 +9,8 @@ class Solution:
             ans += (mem[s]-1)
 
         return ans
+
+        # TC: O(n)
+        # SC: O(1)
+        # Approach: since the dominoes value is between 1 to 9, mem will be between 11 -> 99. We can use an array of size 100 and store count. There is a technique, number of pairs increase by v-1, if count is v currently
         
