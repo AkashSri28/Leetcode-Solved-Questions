ba3ca32e53f780dd275fb3304f8d2d16b9d25234
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-05-08 08:14:17 +0530

    Time: 2199 ms (48.5%), Space: 159.2 MB (16.31%) - LeetHub

---
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        heap = []
+        vis = set()
+        nei = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        m, n = len(moveTime), len(moveTime[0])
+
+        dp = [[float('inf') for _ in range(n)] for _ in range(m)]
+
+        heapq.heappush(heap, (0,0,0,0))
+        vis.add((0,0))
+        dis = 0
+
+        while heap:
+            d, r, c, mov = heapq.heappop(heap)
+
+            if r == m-1 and c == n-1:
+                return d
+
+            for n1, n2 in nei:
+                dr = r+n1
+                dc = c+n2
+
+                if dr >= 0 and dr < m and dc >= 0 and dc < n and (dr, dc) not in vis:
+                    vis.add((dr, dc))
+                    new_d = max(d, moveTime[dr][dc])
+                    v = 0
+                    if mov % 2 == 0:
+                        v = 1
+                    else:
+                        v = 2
+                    new_d += v
+
+                    if new_d < dp[dr][dc]:
+                        dp[dr][dc] = new_d
+                        heapq.heappush(heap, (new_d, dr, dc, v))
+
+        return dp[m-1][n-1]
+
+        
