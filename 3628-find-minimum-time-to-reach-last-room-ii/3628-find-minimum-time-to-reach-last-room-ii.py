19d328d744ef4dabf45004905131d0c996bd28f5
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-05-08 08:16:33 +0530

    Time: 2128 ms (49.78%), Space: 155.6 MB (18.02%) - LeetHub

--- 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.py
+++ 3628-find-minimum-time-to-reach-last-room-ii/3628-find-minimum-time-to-reach-last-room-ii.py
@@ -8,11 +8,12 @@ class Solution:
         dp = [[float('inf') for _ in range(n)] for _ in range(m)]
 
         heapq.heappush(heap, (0,0,0,0))
-        vis.add((0,0))
+       
         dis = 0
 
         while heap:
             d, r, c, mov = heapq.heappop(heap)
+            vis.add((r, c))
 
             if r == m-1 and c == n-1:
                 return d
@@ -22,7 +23,6 @@ class Solution:
                 dc = c+n2
 
                 if dr >= 0 and dr < m and dc >= 0 and dc < n and (dr, dc) not in vis:
-                    vis.add((dr, dc))
                     new_d = max(d, moveTime[dr][dc])
                     v = 0
                     if mov % 2 == 0:
@@ -36,5 +36,9 @@ class Solution:
                         heapq.heappush(heap, (new_d, dr, dc, v))
 
         return dp[m-1][n-1]
+
+        # TC: O(mnlogmn)
+        # SC: O(mn)
+        # Approach: Dijkstra with alternate path weight
 
         
