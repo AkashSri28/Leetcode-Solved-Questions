0732489e978b8d293cb07d9a8da7a3c4aa4fa230
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-05-11 08:20:25 +0530

    Time: 0 ms (100%), Space: 18.1 MB (18.18%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.py
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.py
@@ -10,5 +10,9 @@ class Solution:
                 cnt = 0
         return False
 
+        # TC: O(n)
+        # SC: O(1)
+        # Approach: so we maintain a counter of odd number and return True if counter reaches 3. Reset the counter if even number is encountered
+
 
         
