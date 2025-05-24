c66d51de296060be605b4976dd40bd5a1191b83c
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-05-24 14:13:05 +0530

    Time: 383 ms (62.44%), Space: 27.3 MB (28.5%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -2,29 +2,23 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         m, n = len(s), len(p)
 
-        dp = dict()
-        dp[(m,n)] = True
+        dp = [[-1]*(m+1) for _ in range(n+1)]
 
         def match(i, j):
-            if (i, j) not in dp:
-                if i == m:
-                    if p[j] == '*':
-                        dp[(i, j)] = match(i, j+1)
-                    else:
-                        dp[(i, j)] = False
-                elif j == n:
-                    dp[(i, j)] = False
+            if i == n and j == m:
+                return True
+            if i == n:
+                return False
+            if j == m:
+                # Remaining pattern must be all '*'
+                return all(x == '*' for x in p[i:])
+            if dp[i][j] == -1:
+                if p[i] == '*':
+                    dp[i][j] = match(i+1, j)|match(i+1, j+1)|match(i, j+1)
+                elif p[i] == '?' or p[i] == s[j]:
+                    dp[i][j] = match(i+1, j+1)
                 else:
-                    if p[j] == '?':
-                        dp[(i, j)] = match(i+1, j+1)
-                    elif p[j] == '*':
-                        dp[(i, j)] = (match(i, j+1) or match(i+1, j+1) or match(i+1, j))
-                    else:
-                        if s[i] == p[j]:
-                            dp[(i, j)] = match(i+1, j+1)
-                        else:
-                            dp[(i, j)] = False                
-
-            return dp[(i, j)]
+                    dp[i][j] = False
+            return dp[i][j]
 
         return match(0, 0)
