7663a326ec5944c4690f2de6aafc584b68f20654
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-06-10 09:12:16 +0530

    Time: 24 ms (54.76%), Space: 23.4 MB (18.89%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,31 +1,48 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        n = len(gas)
-        if sum(gas) < sum(cost):
-            return -1
+        # n = len(gas)
+        # if sum(gas) < sum(cost):
+        #     return -1
 
-        def check(i):
-            j, m = (i+1)%n, gas[i] - cost[i]
-            while j != i:
-                m += (gas[j] - cost[j])
-                if m < 0:
-                    break
-                j = (j+1)%n
+        # def check(i):
+        #     j, m = (i+1)%n, gas[i] - cost[i]
+        #     while j != i:
+        #         m += (gas[j] - cost[j])
+        #         if m < 0:
+        #             break
+        #         j = (j+1)%n
             
-            return j
+        #     return j
 
-        i = 0
-        while i < n:
+        # i = 0
+        # while i < n:
+        #     diff = gas[i] - cost[i]
+        #     if diff < 0:
+        #         i += 1
+        #         continue
+        #     if check(i) == i:
+        #         return i
+        #     elif check(i) > i:
+        #         i = check(i)+1
+        #     else:
+        #         return -1
+
+        # return -1
+        total_tank = 0   # Total gas in the system
+        curr_tank = 0    # Gas in the tank for current journey
+        start = 0        # Candidate starting station
+
+        for i in range(len(gas)):
             diff = gas[i] - cost[i]
-            if diff < 0:
-                i += 1
-                continue
-            if check(i) == i:
-                return i
-            elif check(i) > i:
-                i = check(i)+1
-            else:
-                return -1
+            total_tank += diff
+            curr_tank += diff
 
-        return -1
+            # If we can't reach the next station
+            if curr_tank < 0:
+                # Start from the next station
+                start = i + 1
+                curr_tank = 0
+
+        return start if total_tank >= 0 else -1
+
         
