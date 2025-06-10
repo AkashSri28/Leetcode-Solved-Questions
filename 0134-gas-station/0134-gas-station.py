450e6d70e9af94287de77f637e4d89673b85f3f7
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-06-10 08:58:56 +0530

    Time: 97 ms (5.05%), Space: 23.4 MB (18.89%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+
+        def check(i):
+            j, m = (i+1)%n, gas[i] - cost[i]
+            while j != i:
+                m += (gas[j] - cost[j])
+                if m < 0:
+                    break
+                j = (j+1)%n
+            
+            return j
+
+        i = 0
+        while i < n:
+            diff = gas[i] - cost[i]
+            if diff < 0:
+                i += 1
+                continue
+            if check(i) == i:
+                return i
+            elif check(i) > i:
+                i = check(i)+1
+            else:
+                return -1
+
+        return -1
+        
