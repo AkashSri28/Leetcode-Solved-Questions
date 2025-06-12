e4b938436ebf483e39f0b9aa29a290af3ea2a957
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-06-12 09:06:43 +0530

    Time: 0 ms (100%), Space: 17.7 MB (48.85%) - LeetHub

---
+++ 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/3747-maximum-difference-between-adjacent-elements-in-a-circular-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxAdjacentDistance(self, nums: List[int]) -> int:
+        n = len(nums)
+        ans = -1
+        for i in range(n):
+            j = (i+1)%n
+            diff = abs(nums[i]-nums[j])
+            ans = max(ans, diff)
+
+        return ans
+
+        
