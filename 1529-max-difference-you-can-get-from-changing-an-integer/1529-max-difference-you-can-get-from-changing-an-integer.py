a21c3f00285d7eec8d5120997dfbd4ad623a0a5c
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-06-15 08:46:28 +0530

    Time: 0 ms (100%), Space: 18 MB (15.31%) - LeetHub

---
+++ 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.py
@@ -0,0 +1,50 @@
+class Solution:
+    def maxDiff(self, num: int) -> int:
+        arr = [int(d) for d in str(num)]
+        
+        x = -1
+        for a in arr:
+            if a != 9:
+                x = a
+                break
+
+        maxi = []
+        for a in arr:
+            if a == x:
+                maxi.append(9)
+            else:
+                maxi.append(a)
+
+        x = -1
+        if arr[0] != 1:
+            x = arr[0]
+        else:
+            for a in arr:
+                if a > 1:
+                    x = a
+                    break
+
+        mini= []
+
+        for a in arr:
+            if a == x:
+                mini.append(0) if x != arr[0] else mini.append(1)
+            else:
+                mini.append(a)
+
+        n = len(arr)
+        ans = [0]*n
+        c = 0
+
+        for i in range(n-1, -1, -1):
+            if maxi[i]-c >= mini[i]:
+                ans[i] = maxi[i] - c - mini[i]
+                c = 0
+            else:
+                ans[i] = maxi[i]*10 - c - mini[i]
+                c = -1
+        return int(''.join(map(str, ans)))
+
+
+
+        
