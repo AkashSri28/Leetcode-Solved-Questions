8c34daa474faeadd18b86be3a59f206d603b26a1
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-06-16 09:19:58 +0530

    Time: 15 ms (73.43%), Space: 19.9 MB (37.18%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,48 +1,15 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        N = len(ratings)
-        # ans = [1]*N
-        # heap = []
+        n = len(ratings)
+        cnt = [1]*n
 
-        # for i, r in enumerate(ratings):
-        #     heapq.heappush(heap, (r, i))
+        for i in range(1, n):
+            if ratings[i] > ratings[i-1]:
+                cnt[i] = 1+cnt[i-1]
 
-        # while heap:
-        #     r, i = heapq.heappop(heap)
-        #     if i-1 >= 0 and ratings[i-1] < ratings[i] and ans[i] <= ans[i-1]:
-        #         ans[i] = 1+ans[i-1]
-        #     if i+1 < N and ratings[i+1] < ratings[i] and ans[i] <= ans[i+1]:
-        #         ans[i] = 1+ans[i+1]
+        for i in range(n-2, -1, -1):
+            if ratings[i] > ratings[i+1] and cnt[i] <= cnt[i+1]:
+                cnt[i] = 1+cnt[i+1]
 
-        # return sum(ans)
-
-        # TC: O(nlogn)
-        # SC: O(n)
-        # Approach: always pick lowest value and update it according to its neighbours
-
-        left = [1]*N
-        right = [1]*N
-        res = 0
-
-        for i in range(1, N):
-            if ratings[i-1] < ratings[i]:
-                left[i] = 1+left[i-1]
-
-        for i in range(N-2, -1, -1):
-            if ratings[i+1] < ratings[i]:
-                right[i] = 1+right[i+1]
-
-        for a, b in zip(left, right):
-            res += max(a, b)
-
-        return res
-
-        # TC: O(n)
-        # SC: O(n)
-        # Approach: first satisfy for left side elements then satisfy for right side elements
-
-
-
-
-
+        return sum(cnt)
         
