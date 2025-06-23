6073aa2ccb8c15ccc5d325c96bc8b2dbed0f3dd7
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-06-23 08:19:16 +0530

    Time: 1453 ms (88.95%), Space: 18.2 MB (14.29%) - LeetHub

--- 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.py
+++ 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.py
@@ -1,29 +1,38 @@
 class Solution:
     def maxDistance(self, s: str, k: int) -> int:
-        ans = 0
-        mem = {'N': 0, 'S': 0, 'E': 0, 'W': 0}
+        # ans = 0
+        # mem = {'N': 0, 'S': 0, 'E': 0, 'W': 0}
 
-        def find_MD():
-            return abs(mem['N']-mem['S']) + abs(mem['E']-mem['W'])
+        # def find_MD():
+        #     return abs(mem['N']-mem['S']) + abs(mem['E']-mem['W'])
 
-        for i in range(len(s)):
-            ch = s[i]
-            mem[ch] += 1
-            md = find_MD()
-            steps = i+1
+        # for i in range(len(s)):
+        #     ch = s[i]
+        #     mem[ch] += 1
+        #     md = find_MD()
+        #     steps = i+1
 
-            if steps > md:
-                diff = steps-md
-                add = min(2*k, diff)
-                md = md+add
+        #     if steps > md:
+        #         diff = steps-md
+        #         add = min(2*k, diff)
+        #         md = md+add
 
-            ans = max(ans, md)
+        #     ans = max(ans, md)
 
-        return ans
+        # return ans
 
         # TC: O(N)
         # SC: O(1)
         # Approach: Find MD while moving from left to right, whenever MD is less than steps, we can use k there. Max 2*k can be added to MD and we have steps-MD available to update
+
+        lat = lon = ans = 0
+        for i, ch in enumerate(s):
+            if ch == 'N':   lat += 1
+            elif ch == 'S': lat -= 1
+            elif ch == 'E': lon += 1
+            elif ch == 'W': lon -= 1
+            ans = max(ans, min(abs(lat) + abs(lon) + k*2, i+1))
+        return ans
 
 
         
