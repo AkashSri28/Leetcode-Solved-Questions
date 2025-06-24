13ae0646d601659f1d1b1aed36570701c87cc5c8
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-06-24 08:06:29 +0530

    Time: 0 ms (100%), Space: 17.8 MB (90.91%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.py
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.py
@@ -1,21 +1,32 @@
 class Solution:
     def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
         n = len(nums)
-        temp = [0]*n
+        # temp = [0]*n
 
-        def mark(index):
-            left = max(0, index-k)
-            right = min(n, index+k+1)
-            for i in range(left, right):
-                temp[i] = 1
+        # def mark(index):
+        #     left = max(0, index-k)
+        #     right = min(n, index+k+1)
+        #     for i in range(left, right):
+        #         temp[i] = 1
 
-        for index, num in enumerate(nums):
-            if num == key:
-                mark(index)
+        # for index, num in enumerate(nums):
+        #     if num == key:
+        #         mark(index)
 
-        ans = []
-        for i in range(n):
-            if temp[i] == 1:
-                ans.append(i)
+        # ans = []
+        # for i in range(n):
+        #     if temp[i] == 1:
+        #         ans.append(i)
         
+        # return ans
+        ans = []
+        r = 0
+
+        for i in range(n):
+            if nums[i] == key:
+                left = max(r, i-k)
+                r = min(n, i+k+1)
+                for j in range(left, r):
+                    ans.append(j)
+
         return ans
