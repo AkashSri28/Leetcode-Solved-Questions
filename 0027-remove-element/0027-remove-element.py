7aa198af88793d5bf0191dd0619995b6a25a2c2a
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-06-25 17:28:56 +0530

    Time: 0 ms (100%), Space: 17.9 MB (11.14%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,23 +1,10 @@
-class Solution(object):
-    def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
-        i, length = 0, len(nums)
-        j = length
-        
-        while i<j:
-            if nums[i] == val:
-                j -= 1
-                temp = nums[j]
-                nums[j] = nums[i]
-                nums[i] = temp
-            else:
-                i += 1
-        return j
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
 
-# TC: O(n)
-# SC: O(1)
-# Approach: Variation of DNF algorithm, keep j to track val values seen so far
+        return j
+        
