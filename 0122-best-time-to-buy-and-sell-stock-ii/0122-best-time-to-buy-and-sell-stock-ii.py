d4c4522daae67a5a7b6309589f31c26c9594998d
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-06-26 13:00:37 +0530

    Time: 0 ms (100%), Space: 18.7 MB (69.86%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,18 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
-        dp = [[float('-inf')]*2 for _ in range(n)]
-        
-        def helper(day, can_buy):
-            if day == n:
-                return 0
-            if dp[day][can_buy] == float('-inf'):
-                if can_buy == 1:
-                    dp[day][can_buy] = max(-prices[day]+helper(day+1, 0),  helper(day+1, 1))
-                else:
-                    dp[day][can_buy] = max(prices[day]+helper(day+1, 1),  helper(day+1, 0))
-            return dp[day][can_buy]   
-            
-        
-        return helper(0, 1)
+        profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += (prices[i]-prices[i-1])
+
+        return profit
         
