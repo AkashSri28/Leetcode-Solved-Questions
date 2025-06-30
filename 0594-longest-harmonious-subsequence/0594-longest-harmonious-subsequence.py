e97b5b1c9f52c36da84c6026ac7c3e6db7a06acd
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-06-30 07:23:19 +0530

    Time: 24 ms (70.9%), Space: 19.1 MB (46.65%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -1,21 +1,11 @@
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
-        n = len(nums)
-        nums.sort()
-        first_index, last_index = dict(), dict()
-        
-        for i in range(n):
-            if nums[i] not in first_index:
-                first_index[nums[i]] = i
-
-        for i in range(n-1, -1, -1):
-            if nums[i] not in last_index:
-                last_index[nums[i]] = i
-
+        freq = Counter(nums)
         ans = 0
 
-        for key in first_index.keys():
-            if key+1 in last_index:
-                ans = max(ans, last_index[key+1]-first_index[key]+1)
+        for k in freq.keys():
+            if k+1 in freq:
+                ans = max(ans, freq[k]+freq[k+1])
 
         return ans
+
