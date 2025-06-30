b37bd8efa730a0969caaab241e3c89fe67d8390f
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-06-30 07:52:59 +0530

    Time: 2 ms (78.08%), Space: 18.8 MB (30.02%) - LeetHub

--- 0280-wiggle-sort/0280-wiggle-sort.py
+++ 0280-wiggle-sort/0280-wiggle-sort.py
@@ -3,12 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        for i in range(1, n):
+        for i in range(1, len(nums)):
             if i&1 == 1:
                 if nums[i] < nums[i-1]:
                     nums[i], nums[i-1] = nums[i-1], nums[i]
             else:
                 if nums[i] > nums[i-1]:
                     nums[i], nums[i-1] = nums[i-1], nums[i]
+
+        # TC: O(n)
+        # SC: O(1)
+        # Approach: lets take [3,5,2,1,6,4], odd index will have higher values and even index will have lower value. Now check for every index if this condition is not satisfied, swap with previous element.
+        # 1 < 2 out of place, if we swap it will not impact as 1 < 2 and 2 < 5, so swapping them will not impact 5
+        # similarly 6 > 2, swap needed, but since 6>2 and 6>1, so no impact on 1, hence we can swap
         
