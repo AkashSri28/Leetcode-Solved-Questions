706f92cef15b37f69fcf1448434bfa0041e0462e
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-07-01 08:29:23 +0530

    Time: 38 ms (67.89%), Space: 17.9 MB (6.48%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.py
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.py
@@ -1,9 +1,13 @@
 class Solution:
     def possibleStringCount(self, word: str) -> int:
-        ans = 0
+        ans = 1
         for i in range(1, len(word)):
             if word[i] == word[i-1]:
                 ans += 1
 
-        return ans+1
+        return ans
+
+        # TC: O(n)
+        # SC: O(1)
+        # Approach: in abbcccc, ab_c___ is fixed. We have 4 places to fill and we can skip 1 place at a time. Therefore, we have 4 choices here. Also, we can include original word. So total choices 4+1 = 5
         
