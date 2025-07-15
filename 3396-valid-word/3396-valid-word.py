7c15dc51bd581fc480db89991365c4bacc370abe
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-07-15 23:11:11 +0530

    Time: 0 ms (100%), Space: 18 MB (9.76%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, word: str) -> bool:
+        v, c = False, False
+        if len(word) < 3:
+            return False
+        for ch in word:
+            if not ch.isdigit() and not ch.isalpha():
+                return False
+            if ch.isalpha():
+                if ch in 'aeiou' or ch in 'AEIOU':
+                    v = True
+                else:
+                    c = True
+        return (v and c)
+            
+            
+
+        
