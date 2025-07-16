6168e32cd184115bb4132aaf55298a246f752518
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-07-16 11:13:45 +0530

    Time: 53 ms (74.42%), Space: 38.8 MB (95.35%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.py
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.py
@@ -13,4 +13,9 @@ class Solution:
                 cnt += 1 
 
         return max(count_odd, count_even, cnt)
+
+        # TC: O(n)
+        # SC: O(1)
+        # Approach: since we need pairs where sum has same parity. Sum can be even or odd, for even sum we need both elements either odd or both even. If we take either odd or even pair, all elements need to be odd or even pair. So count all odds and even numbers.
+        # Second part, when we need to find alternate pattern, first number will always be included, so we keep adding new numbers and updating valid previous. This will help in finding longest alternate pair chain
         
