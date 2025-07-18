9644b8f25cd07caeba6d1f5a76bbe09a53cdb989
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-07-18 21:08:55 +0530

    Time: 494 ms (39.77%), Space: 55.2 MB (9.94%) - LeetHub

---
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/2267-minimum-difference-in-sums-after-removal-of-elements.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minimumDifference(self, nums: List[int]) -> int:
+        n = len(nums)//3
+        left = [0]*(n*2)
+        min_heap = []
+
+        s = 0
+        for i in range(2*n):
+            s += nums[i]
+            heapq.heappush(min_heap, -nums[i])
+            if len(min_heap) > n:
+                num = heapq.heappop(min_heap)
+                s -= -num
+            left[i] = s
+
+        max_heap = []
+        s = 0
+        right = [0]*(3*n)
+        for i in range(3*n-1, n-1, -1):
+            s += nums[i]
+            heapq.heappush(max_heap, nums[i])
+            if len(max_heap) > n:
+                num = heapq.heappop(max_heap)
+                s -= num
+            right[i] = s
+        
+        ans = float('inf')
+        for i in range(n-1, 2*n):
+            ans = min(ans, left[i]-right[i+1])
+
+        return ans
