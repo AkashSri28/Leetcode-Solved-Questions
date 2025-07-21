b4bfb7fa986d9ded7b147448982e32e519d76809
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-07-21 08:52:37 +0530

    Time: 2 ms (73.62%), Space: 17.7 MB (93.62%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -1,44 +1,25 @@
-# class Solution:
-#     def removeDuplicateLetters(self, s: str) -> str:
-#         stack = []
-#         mem = Counter(s)
-#         seen = set()
-
-#         for i, ch in enumerate(s):
-#             if ch not in seen:
-#                 while stack and stack[-1] > ch and mem[stack[-1]] > 0:
-#                     seen.discard(stack.pop())
-#                 stack.append(ch)
-#                 mem[ch] -= 1
-#                 seen.add(ch)
-#         return ''.join(stack)
-
 class Solution:
-    def removeDuplicateLetters(self, s) -> str:
+    def removeDuplicateLetters(self, s: str) -> str:
+        used = [False]*26
+        left = [0]*26
+
+        for ch in s:
+            left[ord(ch)-ord('a')] += 1
 
         stack = []
+        for ch in s:
+            if used[ord(ch)-ord('a')]:
+                left[ord(ch)-ord('a')] -= 1
+                continue
+                
+            while stack and stack[-1] > ch and left[ord(stack[-1])-ord('a')] > 0:
+                used[ord(stack[-1])-ord('a')] = False
+                stack.pop()
 
-        # this lets us keep track of what's in our solution in O(1) time
-        seen = set()
+            stack.append(ch)
+            left[ord(ch)-ord('a')] -= 1
+            used[ord(ch)-ord('a')] = True
 
-        # this will let us know if there are no more instances of s[i] left in s
-        last_occurrence = {c: i for i, c in enumerate(s)}
-
-
-        for i, c in enumerate(s):
-
-            # we can only try to add c if it's not already in our solution
-            # this is to maintain only one of each character
-            if c not in seen:
-                # if the last letter in our solution:
-                #    1. exists
-                #    2. is greater than c so removing it will make the string smaller
-                #    3. it's not the last occurrence
-                # we remove it from the solution to keep the solution optimal
-                while stack and c < stack[-1] and i < last_occurrence[stack[-1]]:
-                    seen.discard(stack.pop())
-                seen.add(c)
-                stack.append(c)
         return ''.join(stack)
 
         
