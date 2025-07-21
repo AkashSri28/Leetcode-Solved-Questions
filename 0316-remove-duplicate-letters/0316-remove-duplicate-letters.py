54f13604df8dc94fa8e2f56bd1bd2e8fa296ebc5
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-07-21 08:57:00 +0530

    Time: 3 ms (64.21%), Space: 17.8 MB (44.93%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -11,7 +11,7 @@ class Solution:
             if used[ord(ch)-ord('a')]:
                 left[ord(ch)-ord('a')] -= 1
                 continue
-                
+
             while stack and stack[-1] > ch and left[ord(stack[-1])-ord('a')] > 0:
                 used[ord(stack[-1])-ord('a')] = False
                 stack.pop()
@@ -21,6 +21,11 @@ class Solution:
             used[ord(ch)-ord('a')] = True
 
         return ''.join(stack)
+
+        # TC: O(n)
+        # SC: O(n)
+        # Approach: since we need to maintain increasing order we will use montonic array. Now we need to check some conditions before popping element from stack. It should occur in future, else it will be lost forever. For that we use left array. Also we cant use duplicate element in monotonic stack, so we use used array to remember already used element
+
 
         
 
