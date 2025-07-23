95f9637c8faa3c6a8a9ce92bc0275186cd2e7ebf
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-07-23 23:36:21 +0530

    Time: 263 ms (39.35%), Space: 18.9 MB (90.97%) - LeetHub

---
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        n = len(s)
+        ans = 0
+
+        s1, s2 = "ab", "ba"
+        if y > x:
+            s1, s2 = s2, s1
+
+        def find_score(s1, x):
+            nonlocal s
+            stack = []
+            res = 0
+            for ch in s:
+                if stack and stack[-1]+ch == s1:
+                    res += x
+                    stack.pop()
+                    continue
+                stack.append(ch)
+            s = ''.join(stack)
+            return res
+
+        ans += find_score(s1, max(x, y))
+        ans += find_score(s2, min(x, y))
+
+        return ans
+        
