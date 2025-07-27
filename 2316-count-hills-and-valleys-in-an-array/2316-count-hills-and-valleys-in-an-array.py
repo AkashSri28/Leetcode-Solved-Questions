e657288007ca53987f8489c6450a50a3a242f446
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-07-27 08:59:51 +0530

    Time: 0 ms (100%), Space: 17.7 MB (90.84%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
@@ -11,3 +11,7 @@ class Solution:
                 cnt += 1
             p = nums[i]
         return cnt
+
+        # TC: O(n)
+        # SC: O(1)
+        # Approach: We need to cnt values only once if consecutive values are equal. Will move from left to right and store prev non equal value, now for current will move to rightmost index if values are equal. This way i+1 will give next non equal value.
