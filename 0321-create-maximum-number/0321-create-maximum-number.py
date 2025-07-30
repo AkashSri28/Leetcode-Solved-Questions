757c3d4badd22ea46c7f843da7adc68a83249232
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-07-30 15:27:01 +0530

    Time: 4986 ms (5.02%), Space: 18 MB (58.6%) - LeetHub

--- 0321-create-maximum-number/0321-create-maximum-number.py
+++ 0321-create-maximum-number/0321-create-maximum-number.py
@@ -1,28 +1,34 @@
 class Solution:
     def maxNumber(self, nums1: List[int], nums2: List[int], k: int) -> List[int]:
-        def getMaxSubsequence(nums, t):
-            stack = []
-            drop = len(nums) - t
-            for num in nums:
-                while drop and stack and stack[-1] < num:
-                    stack.pop()
-                    drop -= 1
-                stack.append(num)
-            return stack[:t]
+        ans = []
 
-        def merge(seq1, seq2):
+        def pick_max(nums, i):
+            stack = []
+            dis = 0
+            for num in nums:
+                while stack and stack[-1] < num and dis < len(nums) - i:
+                    stack.pop()
+                    dis += 1
+                stack.append(num)
+            return stack[:i]
+
+        def merge(max1, max2):
             res = []
-            while seq1 or seq2:
-                if seq1 > seq2:
-                    res.append(seq1.pop(0))
+            max1, max2 = deque(max1), deque(max2)
+            while max1 or max2:
+                if list(max1) > list(max2):
+                    res.append(max1.popleft())
                 else:
-                    res.append(seq2.pop(0))
+                    res.append(max2.popleft())
             return res
 
-        max_result = []
-        for i in range(max(0, k - len(nums2)), min(k, len(nums1)) + 1):
-            part1 = getMaxSubsequence(nums1, i)
-            part2 = getMaxSubsequence(nums2, k - i)
-            merged = merge(part1[:], part2[:])
-            max_result = max(max_result, merged)
-        return max_result
+        for i in range(k+1):
+            j = k-i
+            max1 = pick_max(nums1, i)
+            max2 = pick_max(nums2, j)
+            merged = merge(max1, max2)
+            if len(merged) == k:
+                ans = max(ans, merged)
+
+        return ans
+
