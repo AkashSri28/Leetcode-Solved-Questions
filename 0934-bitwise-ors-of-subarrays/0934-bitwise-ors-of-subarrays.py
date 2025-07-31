9d6288350eac8b8195a83c3d09452501ceff8746
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-07-31 07:37:58 +0530

    Time: 410 ms (68.45%), Space: 44.8 MB (69.64%) - LeetHub

--- 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.py
+++ 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.py
@@ -13,3 +13,6 @@ class Solution:
 
         return len(res)
 
+        # TC: O(n*32)
+        # SC: O(32)
+        # Approach: For curr number, we can Or it with Or of all prev results. And store unique values, in worst cast 32 values will be stored. So the solution will be sufficient
