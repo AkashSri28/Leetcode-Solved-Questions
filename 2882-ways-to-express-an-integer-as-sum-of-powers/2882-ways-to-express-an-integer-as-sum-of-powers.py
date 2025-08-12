a983cb5dd75b9792136c762c2c60c22cc34d2c40
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-08-12 09:12:55 +0530

    Time: 286 ms (82.85%), Space: 17.7 MB (93.02%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.py
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.py
@@ -1,19 +1,36 @@
 class Solution:
     def numberOfWays(self, n: int, x: int) -> int:
-        mod = 10**9+7
-        mem = dict()
+        MOD = 10**9+7
+        # mem = dict()
 
-        def solve(n, num):
-            if n < 0 or num**x > n:
-                return 0
-            if n == num**x:
-                return 1
+        # def solve(n, num):
+        #     if n < 0 or num**x > n:
+        #         return 0
+        #     if n == num**x:
+        #         return 1
 
-            if (n, num) not in mem:
-                a = solve(n - num**x, num+1)
-                b = solve(n, num+1)
-                mem[(n, num)] = (a+b)%mod
-            return mem[(n, num)]
+        #     if (n, num) not in mem:
+        #         a = solve(n - num**x, num+1)
+        #         b = solve(n, num+1)
+        #         mem[(n, num)] = (a+b)%mod
+        #     return mem[(n, num)]
 
-        return solve(n, 1)
+        # return solve(n, 1)
+
+        powers = []
+        i = 1
+        while True:
+            p = pow(i, x)
+            if p > n:
+                break
+            powers.append(p)
+            i += 1
+
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        for p in powers:
+            for s in range(n, p - 1, -1):
+                dp[s] = (dp[s] + dp[s - p]) % MOD
+
+        return dp[n]
         
