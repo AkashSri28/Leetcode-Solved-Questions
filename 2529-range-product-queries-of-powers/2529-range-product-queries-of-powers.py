6358c6df57199aafe229be0a31f7749af0826629
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-08-13 09:11:14 +0530

    Time: 247 ms (12.28%), Space: 48.3 MB (6.42%) - LeetHub

--- 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.py
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.py
@@ -2,21 +2,22 @@ class Solution:
     def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
         mod = 1000000007
         i = 0
-        state = [1]
+        state = []
         while n > 0:
             if n & 1 == 1:
-                state.append(pow(2, i))
-                state[-1] *= state[-2]
+                state.append(pow(2, i)%mod)
             i += 1
             n = n>>1
 
-        print(state)
+        # print(state)
 
         ans = []
 
         for (s, e) in queries:
-            num = state[e+1] * pow(state[s], mod-2, mod) % mod
-            # print(num)
+            num = 1
+            for i in range(s, e+1):
+                num *= state[i]
+                num %= mod
             ans.append(num)
 
         return ans
