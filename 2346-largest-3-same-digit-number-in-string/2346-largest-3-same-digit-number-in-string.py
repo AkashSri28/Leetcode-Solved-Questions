9add3cd570e172b8f0a47646de3723eabea9c743
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-08-15 13:24:26 +0530

    Time: 3 ms (67.45%), Space: 17.6 MB (87.48%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.py
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.py
@@ -1,41 +1,15 @@
 class Solution:
     def largestGoodInteger(self, num: str) -> str:
-        # if len(num) < 3:
-        #     return ""
-        # ans = -1
-
-        # n = len(num)
-        # mem = dict()
-
-        # for i in range(3):
-        #     if num[i] not in mem:
-        #         mem[num[i]] = 0
-        #     mem[num[i]] += 1
-            
-        # if len(mem) == 1 and int(num[i]) > ans:
-        #     ans = int(num[i])
-
-        # for i in range(3, n):
-        #     j = i-3
-        #     if num[i] not in mem:
-        #         mem[num[i]] = 0
-        #     mem[num[i]] += 1
-
-        #     mem[num[j]] -= 1
-        #     if mem[num[j]] == 0:
-        #         del mem[num[j]]
-
-        #     if len(mem) == 1 and int(num[i]) > ans:
-        #         ans = int(num[i])
-
-        # return "" if ans == -1 else str(ans)*3
-
         best = None
         for i in range(len(num) - 2):
             if num[i] == num[i + 1] == num[i + 2]:
                 if best is None or num[i] > best:  # char comparison works for digits
                     best = num[i]
         return "" if best is None else best * 3
+
+        # TC: O(n)
+        # SC: O(1)
+        # Approach: From left to right, for each valid index check i, i+1, i+2. If they are same and greater than best. Update your best.
 
 
 
