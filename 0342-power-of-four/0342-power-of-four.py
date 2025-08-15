420cab63f2dc701bd0546bd06c1965d7adecc45f
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-08-15 13:34:44 +0530

    Time: 0 ms (100%), Space: 17.6 MB (88.88%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,4 +1,8 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         return n in [1, 4, 16, 64, 256, 1024, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216, 67108864, 268435456, 1073741824]
+
+        # TC: O(1)
+        # SC: O(1)
+        # Approach: Find all powers of 4 before and store. Now check if n exists there
         
