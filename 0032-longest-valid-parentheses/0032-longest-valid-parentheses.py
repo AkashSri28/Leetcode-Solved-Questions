6054da1ce7ae705d63a23ad7f8a95384966b14df
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-08-15 14:42:55 +0530

    Time: 7 ms (63.06%), Space: 18.2 MB (74.45%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -14,3 +14,12 @@ class Solution:
 
                 ans = max(ans, dp[i])
         return ans
+
+        # TC: O(n)
+        # SC: O(n)
+        # Approach: Find length of longest valid paranthesis ending at index i. So we will check only if i has ')'.
+        # Now if i-1 is '(', we will find dp[i-2]+2        ....()
+        # else if i-1 is ')'
+        # then there will be 2 cases
+        #     ....(()) or ....)())
+        #     so we will check at dp[i-1-dp[i-1]] if that is ( then we will check 2+dp[i-1]+dp[i-2-dp[i-1]]
