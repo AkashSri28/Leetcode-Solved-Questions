03b92b30d41f7b01686307a8f830cba90788d30d
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-08-17 09:30:22 +0530

    Time: 39 ms (15.16%), Space: 19.3 MB (5.81%) - LeetHub

--- 0867-new-21-game/0867-new-21-game.py
+++ 0867-new-21-game/0867-new-21-game.py
@@ -14,4 +14,8 @@ class Solution:
             window_sum = window_sum + dp[i] - dp[i+maxPts]
 
         return dp[0]
+
+        # TC: O(k+maxPts)
+        # SC: O(k+maxPts)
+        # Approach: We need to find probability of reaching a node starting from 0. If the leaf node is between k and n, we return 1, if above n, we return 0. If below k we find again. Lets reverse the solution, we start writing probability from back. So for all nodes between k and n, probability will be 1, above n till maxPts it will be 0. Now for k-1 and below, maintain a running sum and calculate probability
         
