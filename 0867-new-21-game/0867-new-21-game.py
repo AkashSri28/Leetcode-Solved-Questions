cd0152b9deb1e1eea206fe7bbdb61c79e385fbfa
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-08-17 09:31:46 +0530

    Time: 23 ms (72.9%), Space: 18.2 MB (82.26%) - LeetHub

--- 0867-new-21-game/0867-new-21-game.py
+++ 0867-new-21-game/0867-new-21-game.py
@@ -1,13 +1,11 @@
 class Solution:
     def new21Game(self, n: int, k: int, maxPts: int) -> float:
-        dp = dict()
+        dp = [0]*(k+maxPts)
         window_sum = 0
         for i in range(k, k+maxPts):
             if i <= n:
                 dp[i] = 1
                 window_sum += 1
-            else:
-                dp[i] = 0
 
         for i in range(k-1, -1, -1):
             dp[i] = window_sum/maxPts
