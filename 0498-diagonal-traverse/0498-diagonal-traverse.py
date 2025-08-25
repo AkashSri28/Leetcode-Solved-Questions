6de37bd758cc7c783210b863f8fe2130efad1dc8
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-08-25 08:39:33 +0530

    Time: 9 ms (69.97%), Space: 19.7 MB (59.95%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -21,4 +21,8 @@ class Solution:
 
         return res
 
+        # TC: O(m**2+n**2)
+        # SC: O(1)
+        # Approach: i will move from 0 to m+n-1. When i is even, move up, when i is odd move down. Check boundary conditions.
+
         
