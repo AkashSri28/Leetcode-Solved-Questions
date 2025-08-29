468b5c6596b34c0378289a6d7bd7d7ff7abe310b
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-08-29 09:18:08 +0530

    Time: 0 ms (100%), Space: 17.7 MB (61.11%) - LeetHub

--- 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.py
+++ 3279-alice-and-bob-playing-flower-game/3279-alice-and-bob-playing-flower-game.py
@@ -3,4 +3,9 @@ class Solution:
         on, en = (n + 1) // 2, n // 2
         om, em = (m + 1) // 2, m // 2
         return on * em + en * om
+
+
+    # TC: O(1)
+    # SC: O(1)
+    # Approach: Alice will win when flowers in lane x and greater than y and vice versa. Count all possiblities when x is odd and greater and y is even and smaller
         
