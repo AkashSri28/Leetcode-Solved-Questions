2b4816dbba18550875136a3e1de619ff66da4580
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-08-30 09:09:44 +0530

    Time: 9 ms (13.02%), Space: 17.7 MB (92.34%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        def validMatrix(r1, r2, c1, c2):
+            temp = [0]*10
+            for i in range(r1, r2+1):
+                for j in range(c1, c2+1):
+                    val = board[i][j]
+                    if val == ".":
+                        continue
+                    val = int(val)
+                    temp[val] += 1
+                    if temp[val] > 1:
+                        return False
+            return True
+
+
+        for i in range(9):
+            if not validMatrix(i, i, 0, 8):
+                return False
+
+        for j in range(9):
+            if not validMatrix(0, 8, j, j):
+                return False
+
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                if not validMatrix(i, i+2, j, j+2):
+                    return False
+
+        return True
+        
