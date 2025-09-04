6a5b6c165bd6a48c183b404a4c7eb1fd0df2b59f
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-09-04 07:37:31 +0530

    Time: 0 ms (100%), Space: 17.7 MB (52.11%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.py
+++ 3830-find-closest-person/3830-find-closest-person.py
@@ -6,4 +6,8 @@ class Solution:
         if d1 < d2:
             return 2
         return 0
+
+        # TC: O(1)
+        # SC: O(1)
+        # Approach: calculate abs distance and return min
         
