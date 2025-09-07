0ad3537839dbbedec4a7a92653d1a9bf1887ab7f
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-09-07 09:11:42 +0530

    Time: 0 ms (100%), Space: 17.8 MB (56.31%) - LeetHub

--- 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.py
+++ 2837-minimum-operations-to-make-the-integer-zero/2837-minimum-operations-to-make-the-integer-zero.py
@@ -8,5 +8,21 @@ class Solution:
                 return -1
         return -1
 
+        # TC: O(1)
+        # SC: O(1)
+        # Approach: num1' = num1 - num2 - 2^i
+        #           num1'' = num1' - num2 - 2^i
+        #           num1''' = num1'' - num2 - 2^i
+        #                 = num1' - num2 - 2^i - num2 - 2^i
+        #                 = num1 - num2 - 2^i- num2 - 2^i - num2 - 2^i
+        #                 = num1 - 3*num2 - (2^i + 2^j + 2^k)
+        #         now if we want to make num1''' as 0
+        #         0 = num1 - 3*num2 - (2^i + 2^j + 2^k)
+        #         (2^i + 2^j + 2^k) = num1 - t*num2
+
+        #         so we take value of t from 1 to 60 and check when number of 2^i terms are suitable
+        #         min will be number of set bits, max will be val (2^0, sum of all 1s), so if our value lies between this then we have answer
+
+
         
         
