ee9cbe5c1d793d6b4e8073d8317e76b74d6ec993
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-09-08 08:44:56 +0530

    Time: 0 ms (100%), Space: 17.9 MB (31.73%) - LeetHub

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.py
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.py
@@ -3,4 +3,8 @@ class Solution:
         for i in range(1, n):
             if '0' not in str(i) and '0' not in str(n-i):
                 return [i, n-i]
+
+        # TC: O(n)
+        # SC: O(1)
+        # Approach: From i to n-1, check all pairs and find a pair where 0 not present in any element from pair
         
