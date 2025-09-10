862d66fc5a793b860439879e45d808d9eeeb0b39
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-09-10 12:46:43 +0530

    Time: 255 ms (22.46%), Space: 17.8 MB (72.46%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.py
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.py
@@ -1,24 +1,22 @@
 from collections import deque
 class Solution:
     def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
-        MOD = 10**9 + 7
-        dp = [0] * (n + 1)  # dp[i] = number of people who learn on day i
+        mod = 10**9+7
+        dp = [0]*(n+1)
         dp[1] = 1
-        share_sum = 0  # running sum of people currently sharing
-        for i in range(2, n + 1):
-            # add those who start sharing today
-            if i - delay >= 1:
-                share_sum = (share_sum + dp[i - delay]) % MOD
-            # remove those who forget today (they stop sharing)
-            if i - forget >= 1:
-                share_sum = (share_sum - dp[i - forget]) % MOD
-            dp[i] = share_sum
-
-        # count those who still remember on day n (learned in last forget-1 days)
+        for i in range(2, n+1):
+            cnt = 0
+            for j in range(i-forget+1, i-delay+1):
+                if j < 0:
+                    continue
+                cnt = (cnt+dp[j])%mod
+            dp[i] = cnt
         ans = 0
-        for i in range(max(1, n - forget + 1), n + 1):
-            ans = (ans + dp[i]) % MOD
+        for i in range(n-forget+1, n+1):
+            ans = (ans+dp[i])%mod
+
         return ans
+
 
 
         
