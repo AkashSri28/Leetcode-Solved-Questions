f5536c4c42d472b81fa233294e35abb8af9853c6
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-09-12 06:07:47 +0530

    Time: 17 ms (75.15%), Space: 18.1 MB (50.3%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.py
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.py
@@ -1,10 +1,7 @@
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
         vowels = 'aeiou'
-        chars = list(s)
-        cnt = 0
         for ch in s:
             if ch in vowels:
-                cnt += 1
                 return True
         return False
