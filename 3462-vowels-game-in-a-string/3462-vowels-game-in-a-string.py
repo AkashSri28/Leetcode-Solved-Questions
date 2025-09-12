1d1c087aa68f9ec90def5b483d8c2c5e8960f02b
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-09-12 06:10:34 +0530

    Time: 18 ms (74.55%), Space: 18.3 MB (7.58%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.py
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.py
@@ -5,3 +5,8 @@ class Solution:
             if ch in vowels:
                 return True
         return False
+
+        # TC: O(n)
+        # SC: O(1)
+        # Approach: basically alice will take max possible vowels in first turn. So if there are odd number of vowels, alice will take everything in first turn, then bob will have nothing to pick, alice wins. if there are even vowels, alice will take total - 1 vowels, bob cant pick 1 vowel, hence alice wins again. 
+        # Only case when bob can win is when there are no vowels, alice will have no move. So check if there is any vowel in string, return True, else false
