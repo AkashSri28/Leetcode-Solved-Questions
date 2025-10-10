b892f2abdc76aa71c600f2350c202c7951fb51eb
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-10-10 07:57:53 +0530

    Time: 27 ms (27.96%), Space: 19.3 MB (69.76%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,31 +1,20 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        max_height_left = [0]*len(height)
-        max_height_right = [0]*len(height)
-        res = 0
-        
-        max_height = height[0]
-        for i in range(1, len(height)):
-            max_height_left[i] = max_height
-            max_height = max(max_height, height[i])
-            
-        max_height = height[-1]
-        for i in range(len(height) - 2, -1, -1):
-            max_height_right[i] = max_height
-            max_height = max(max_height, height[i])
-            
-        for i in range(len(height)):
-            water_height = min(max_height_left[i], max_height_right[i])
-            if height[i] < water_height:
-                res += water_height - height[i]
-                
-        return res
-    
-# TC: O(n)
-#     SC: O(n) #for storing left max height and right max height
-#         Approach: for every index we need to left and right max possible height
-#             now if min of left and right max height is greater than current height
-#             that means water can be stored
-            
-                
+        n = len(height)
+        rmax = [0]*n
+        ans = 0
+
+        for i in range(n-2, 0, -1):
+            rmax[i] = max(height[i+1], rmax[i+1])
+
+        lmax = height[0]
+        for i in range(1, n-1):
+            h = min(lmax, rmax[i])
+            if h > height[i]:
+                ans += h - height[i]
+            lmax = max(lmax, height[i])
+
+        return ans
+
+
         
