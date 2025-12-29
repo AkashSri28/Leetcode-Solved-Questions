b1f05997940d9d3cf2887188485df1639fea4cb2
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2025-12-29 08:12:11 +0530

    Time: 0 ms (100%), Space: 22.1 MB (87.79%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,6 +8,8 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
+        if not head or not head.next:
+            return
         p1, p2 = head, head.next
         while p1 and p2 and p2.next:
             p1 = p1.next
@@ -29,6 +31,11 @@ class Solution:
             p1 = node.next
             
 
+        # TC: O(2n)
+        # SC: O(n)
+        # Approach: Use tortoise and hare algo to find mid point of linked list
+        # Push all nodes starting from mid + 1 into stack
+        # now pop these nodes from stack and connect to linked list
 
 
         
