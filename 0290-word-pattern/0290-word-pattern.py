3173d885c29ac597c4c478aa5aae90bef68dabf0
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2026-02-21 12:11:20 +0530

    Time: 0 ms (100%), Space: 12.4 MB (58.49%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        mem1, mem2 = {}, {}
+        word_list = s.split(' ')
+
+        if len(pattern) != len(word_list):
+            return False
+
+        for ch, word in zip(pattern, word_list):
+            if ch in mem1:
+                if mem1[ch] != word:
+                    return False
+
+            else:
+                if word in mem2:
+                    return False
+
+                mem1[ch] = word
+                mem2[word] = ch
+
+        return True
+        
