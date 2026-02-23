53329d2214d339ecb1c3d5a4b83655fb50845c89
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2026-02-23 15:29:50 +0530

    Time: 1015 ms (15.75%), Space: 13.1 MB (93.41%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,46 +1,49 @@
-class Solution:
-    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+class Solution(object):
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        n = len(words)
         word_len = len(words[0])
-        n = len(s)
-        word_map = Counter(words)
-        k = len(words)
-        res = []
+        ans = []
+        words_freq = Counter(words)
 
         for i in range(word_len):
             left = right = i
-            seen = Counter()
-            count = 0
-            while right + word_len <= n:
-                word = s[right:right+word_len]
+            cnt = 0
+            mem = defaultdict(int)
 
-                if word in word_map:
-                    seen[word] += 1
-                    count += 1
+            while right + word_len <= len(s):
+                word = s[right: right+word_len]
+                if word in words_freq:
+                    cnt += 1
+                    mem[word] += 1
 
-                    while seen[word] > word_map[word]:
-                        seen_word = s[left: left+word_len]
-                        seen[seen_word] -= 1
-                        count -= 1
+                    while mem[word] > words_freq[word]:
+                        left_word = s[left: left+word_len]
+                        mem[left_word] -= 1
+                        cnt -= 1
                         left += word_len
 
-                    if count == k:
-                        res.append(left)
+                    if cnt == n:
+                        ans.append(left)
 
-                        left_word = s[left:left + word_len]
-                        seen[left_word] -= 1
-                        left += word_len
-                        count -= 1
+                    print(mem, left)
 
                 else:
                     left = right + word_len
-                    seen.clear()
-                    count = 0
-
+                    mem.clear()
+                    cnt = 0
                 right += word_len
 
-        return res
+        return ans
 
-        # TC: O(n)
-        # SC: O(k)
-        # Approach: find the window where all words in words exist. If invalid word is seen, move starting point of window
+                
+
+
+
+
+
         
