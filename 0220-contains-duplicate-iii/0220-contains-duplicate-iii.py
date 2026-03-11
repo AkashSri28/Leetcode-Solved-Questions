d93d6d1639924f8f0993bd556d0dcf219193e189
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2026-03-11 12:24:45 +0530

    Time: 1220 ms (24.53%), Space: 20.9 MB (81.13%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.py
@@ -14,7 +14,7 @@ class Solution(object):
             if r - l > indexDiff:
                 sl.remove(nums[l])
                 l += 1
-                
+
             pos = sl.bisect_left(nums[r]-valueDiff)
             if pos < len(sl) and sl[pos] <= nums[r]+valueDiff:
                 print(nums[r], pos, len(sl))
@@ -23,5 +23,9 @@ class Solution(object):
             sl.add(nums[r])
 
         return False
+
+        # TC: O(nlogk)
+        # SC: O(k)
+        # Approach: use sortedlist to store elements in a sorted list. Now find the element where nums[r]-val can be inserted. Check if there is an element already here. If yes, then check if element <= nums[r]+val
 
         
