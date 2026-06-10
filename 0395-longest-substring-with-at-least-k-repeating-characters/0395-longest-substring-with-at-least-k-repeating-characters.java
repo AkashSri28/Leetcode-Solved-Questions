145b0e3e23a2ce3b0022305c11fd755917122829
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2026-06-10 06:27:07 +0530

    Time: 0 ms (100%), Space: 42.5 MB (93.82%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
@@ -41,5 +41,6 @@ class Solution {
     }
 }
 
-// TC: O(logn*26)
-// SC: O(26)
+// TC: O(n^2) for each character in the string we can have a split, and in each split we will go till n characters
+// SC: O(n) recursion depth
+// Approach: find freq of each char in string, now scan left to right, when you see a char with freq < k, it means this can never be part of the answer. So split the string and check whether split string satisfies the conditions
