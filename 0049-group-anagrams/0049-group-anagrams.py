c5acd3935ca90b7075331aa715362a5f7180143d
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2026-08-08 23:16:05 +0530

    Time: 13 ms (48.96%), Space: 21.9 MB (78.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -14,6 +14,13 @@ class Solution:
 
         return res
 
+# TC: O(n*klogk)
+# SC: O(n*k)
+# Approach: we confirm if all strings are lowercase chars
+# now to group them we need to find something common
+# if two strings are anagrams, their sorted form will be same
+# hence we will sort and check in dict mem
+# for testing we will check with null string as well
 
 
         
