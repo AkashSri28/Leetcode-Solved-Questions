2ad17dca27800de3bb161db72899383044e5f9a1
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2026-08-08 14:18:33 +0530

    Time: 0 ms (100%), Space: 19.5 MB (9.07%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,27 +1,19 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
-        for i in range(m, m+n):
-            nums1[i] = nums2[i-m]
+        i, j, k = m-1, n-1, m+n-1
+        while i >= 0 and j >= 0:
+            if nums1[i] <= nums2[j]:
+                nums1[k] = nums2[j]
+                j -= 1
+            else:
+                nums1[k] = nums1[i]
+                i -= 1
+            k -= 1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            k -= 1
+            j -= 1
         
-        i, j = 0, m
-        while i < m and j < m+n:
-            if nums1[i] > nums1[j]:
-                temp = nums1[i]
-                nums1[i] = nums1[j]
-                k = j+1
-                while k < m+n and temp > nums1[k]:
-                    nums1[k-1] = nums1[k]
-                    k += 1
-                nums1[k-1] = temp   
-            i += 1 
-        
-# TC: O(m*n)
-# SC: O(1)
-    
