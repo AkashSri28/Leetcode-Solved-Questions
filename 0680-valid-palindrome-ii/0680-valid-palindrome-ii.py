921acd53b040a8e9b9d66cd4652df79d3819ab38
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2026-08-10 20:42:42 +0530

    Time: 57 ms (55.93%), Space: 19.6 MB (42.2%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,22 +1,22 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def isPalin(i, j):
-            while i < j:
-                if s[i] != s[j]:
+        l, r = 0, len(s) - 1
+
+        def isPalindrome(s, l, r):
+            while l <= r:
+                if s[l] != s[r]:
                     return False
-                i += 1
-                j -= 1
+                l += 1
+                r -= 1
             return True
 
-        l, r = 0, len(s)-1
-        while l < r:
+        while l <= r:
             if s[l] != s[r]:
-                return isPalin(l+1, r) or isPalin(l, r-1)
+                return isPalindrome(s, l+1, r) | isPalindrome(s, l, r-1)
             l += 1
             r -= 1
-
         return True
 
-        # TC: O(n)
-        # SC: O(1)
-        # Approach: we will try to check palindrome. If not we can check (i+1, j) and (i, j-1). If any of this is palindrome then we got the answer
+
+            
+        
