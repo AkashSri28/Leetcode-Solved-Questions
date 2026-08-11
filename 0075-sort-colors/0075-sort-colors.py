ff28d9a071c6bd68b00e1f8ccc7cb839fe000a81
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2026-08-11 14:00:18 +0530

    Time: 52 ms (2.1%), Space: 19.3 MB (61.53%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -16,5 +16,10 @@ class Solution:
             else:
                 j += 1
         
-
+# Clarification: will clarify if only 3 values exist in nums, size of nums
+# Brute Force: sort O(nlogn)
+# Approach: will use 3 pointers i, j, k, i will point where next 0 will come, k will point where next 2 will come, if we see a 1 will move forward as i will get stuck here
+# TC: O(n)
+# SC: O(1)
+# Testing: check when data is sorted, reverse sorted, only 1 value type is there
         
