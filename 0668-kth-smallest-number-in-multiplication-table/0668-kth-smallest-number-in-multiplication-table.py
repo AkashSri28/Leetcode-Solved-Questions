6080a8433f4378bfbc111dccbf0ed3169a990331
AkashSri28 <61103545+AkashSri28@users.noreply.github.com>
2026-09-03 06:36:55 +0530

    Time: 303 ms (69.11%), Space: 19.3 MB (32.76%) - LeetHub

--- 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.py
+++ 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.py
@@ -14,3 +14,10 @@ class Solution:
                 low = mid + 1
         return ans
         
+# Clarification: can check for range of m and n
+# Brute Force: check all numbers between 1 and m*n, kth element will be ans, TC O(m*n)
+# Approach: so answer will be between 1 and m*n, so BS can be tried here (hint). Now for each row check number of elements that will be less than mid guessed. min(mid // i, n) will give the numbers less than mid in each row. if cnt >= k then move left, else move right.
+# why min(mid // i, n)? since in each row m will be constant, only n will vary, hence dividing mid by m for that row will give position of n, now min(mid//i, n) will tell the value of n if less than n or else it will take n
+# TC: O(n)
+# SC: O(1)
+# Testing:
